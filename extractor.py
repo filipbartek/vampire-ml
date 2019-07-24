279021043c2fecdee239a5940b6406ad5a3a20b6
Filip Bartek <filip.bartek@hotmail.com>
2019-07-24 16:13:23 +0200

    Name each ReExtractor
    for more clear error messages.

--- extractor.py
+++ extractor.py
@@ -5,18 +5,19 @@ import re
 
 
 class ReExtractor:
-    def __init__(self, pattern, convert):
+    def __init__(self, pattern, convert, name):
         """
         :param pattern: A regular expression object.
         :param convert: A function that converts a regular expression match object to the value of this match.
         """
         self.pattern = pattern
         self.convert = convert
+        self.name = name
 
     def __call__(self, s):
         m = self.pattern.search(s)
         if m is None:
-            raise RuntimeError(f'Pattern failed to match.')
+            raise RuntimeError(f'Pattern {self.name} failed to match.')
         return self.convert(m)
 
 
@@ -41,20 +42,23 @@ class MultiExtractor:
 
 vampire_version = ReExtractor(re.compile(
     '^% Version: Vampire (?P<number>[\d\.]*) \(commit (?P<commit_hash>[\da-f]*) on (?P<commit_timestamp>.*)\)$',
-    re.MULTILINE), lambda m: m.groupdict())
-termination_reason = ReExtractor(re.compile('^% Termination reason: (.*)$', re.MULTILINE), lambda m: m[1])
-termination_phase = ReExtractor(re.compile('^% Termination phase: (\w*)$', re.MULTILINE), lambda m: m[1])
+    re.MULTILINE), lambda m: m.groupdict(), 'vampire_version')
+termination_reason = ReExtractor(re.compile('^% Termination reason: (.*)$', re.MULTILINE), lambda m: m[1],
+                                 'termination_reason')
+termination_phase = ReExtractor(re.compile('^% Termination phase: (\w*)$', re.MULTILINE), lambda m: m[1],
+                                'termination_phase')
 function_names = ReExtractor(re.compile('^% Function symbol names: ([\w,]*)$', re.MULTILINE),
-                             lambda m: list(m[1].split(',')))
+                             lambda m: list(m[1].split(',')), 'function_names')
 function_precedence = ReExtractor(
     re.compile('^% Function symbol index precedence permutation: ([\d,]*)$', re.MULTILINE),
-    lambda m: list(map(int, m[1].split(','))))
+    lambda m: list(map(int, m[1].split(','))), 'function_precedence')
 predicate_names = ReExtractor(re.compile('^% Predicate symbol names: ([\w=,]*)$', re.MULTILINE),
-                              lambda m: list(m[1].split(',')))
+                              lambda m: list(m[1].split(',')), 'predicate_names')
 predicate_precedence = ReExtractor(
     re.compile('^% Predicate symbol index precedence permutation: ([\d,]*)$', re.MULTILINE),
-    lambda m: list(map(int, m[1].split(','))))
-time_elapsed = ReExtractor(re.compile('^% Time elapsed: (\d+\.\d+) s$', re.MULTILINE), lambda m: float(m[1]))
+    lambda m: list(map(int, m[1].split(','))), 'predicate_precedence')
+time_elapsed = ReExtractor(re.compile('^% Time elapsed: (\d+\.\d+) s$', re.MULTILINE), lambda m: float(m[1]),
+                           'time_elapsed')
 termination = MultiExtractor({
     'reason': (termination_reason, True),
     'phase': (termination_phase, False)
