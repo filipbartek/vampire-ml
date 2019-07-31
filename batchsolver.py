927f9a6b4e5caa3f1ac3e0beeb9ba5a5ccc67626
Filip Bartek <filip.bartek@hotmail.com>
2019-07-31 18:06:39 +0200

    Parallelize proving runs
    Allow running multiple prove runs on one problem in parallel.
    Use a common task queue for probe and prove runs, prioritizing prove
    runs.

--- batchsolver.py
+++ batchsolver.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3.7
 
 import collections
+import concurrent.futures
 import json
 import logging
-import multiprocessing
 import os
 
 
@@ -23,66 +23,81 @@ class BatchSolver:
         self.time_limit_probe = time_limit_probe
         self.time_limit_solve = time_limit_solve
 
-    @staticmethod
-    def generate_tasks(problem_paths, parameters, outpath):
-        for problem_index, problem_path in enumerate(problem_paths):
-            problem_outpath = os.path.join(outpath, str(problem_index))
-            problem_parameters = get_updated(parameters, {
-                'problem_index': problem_index,
-                'paths': {
-                    'problem': problem_path
-                }
-            })
-            yield (problem_parameters, problem_outpath, outpath)
-
     def solve_problems(self, problem_paths, parameters, outpath, jobs=1):
-        results = []
-        with multiprocessing.Pool(jobs) as p:
-            try:
+        results = dict()
+        try:
+            with concurrent.futures.ThreadPoolExecutor(max_workers=jobs) as executor:
+                futures = set()
+                for problem_index, problem_path in enumerate(problem_paths):
+                    self.reduce_futures(futures, results, outpath, jobs)
+                    problem_outpath = os.path.join(outpath, str(problem_index))
+                    problem_parameters = get_updated(parameters, {
+                        'problem_index': problem_index,
+                        'paths': {
+                            'problem': problem_path
+                        }
+                    })
+                    probe_parameters = get_updated(problem_parameters, {
+                        'probe': True,
+                        'vampire': {
+                            'time_limit': self.time_limit_probe,
+                            'mode': 'clausify'
+                        }
+                    })
+                    prove_parameters = get_updated(problem_parameters, {
+                        'probe': False,
+                        'vampire': {
+                            'time_limit': self.time_limit_solve
+                        }
+                    })
+                    futures.add(executor.submit(self.run_probe, problem_outpath, probe_parameters, prove_parameters,
+                                                parameters['run_count'], executor, futures))
+                self.reduce_futures(futures, results, outpath)
+        finally:
+            if outpath is not None:
                 os.makedirs(outpath, exist_ok=True)
-                with open(os.path.join(outpath, 'prove_runs.txt'), 'w') as runs_file:
-                    for result in p.imap_unordered(self.solve_problem_tuple,
-                                                   self.generate_tasks(problem_paths, parameters, outpath)):
-                        results.append(result)
-                        if len(result['prove_runs']) > 0:
-                            runs_file.write('%s\n' % '\n'.join(result['prove_runs']))
-            finally:
-                if outpath is not None:
-                    os.makedirs(outpath, exist_ok=True)
-                    with open(os.path.join(outpath, 'result.json'), 'w') as result_file:
-                        json.dump(results, result_file, indent=4)
+                with open(os.path.join(outpath, 'result.json'), 'w') as result_file:
+                    json.dump(results, result_file, indent=4)
+        return results
 
-    def solve_problem_tuple(self, args):
-        return self.solve_problem(*args)
+    @staticmethod
+    def reduce_futures(futures, results, base_path, n=0):
+        while len(futures) > n:
+            done, _ = concurrent.futures.wait(list(futures), return_when=concurrent.futures.FIRST_COMPLETED)
+            for future in done:
+                assert future.done()
+                result = future.result()
+                problem_index = result['parameters']['problem_index']
+                data_path = os.path.relpath(os.path.join(result['parameters']['paths']['base'],
+                                                         result['parameters']['paths']['data']), start=base_path)
+                if problem_index not in results:
+                    results[problem_index] = {
+                        'prove_runs': []
+                    }
+                if result['parameters']['probe']:
+                    results[problem_index]['probe'] = data_path
+                else:
+                    results[problem_index]['prove_runs'].append(data_path)
+                futures.remove(future)
 
-    def solve_problem(self, parameters, outpath, base_path=None):
-        result = {
-            'outpath': os.path.relpath(outpath, start=base_path),
-            'probe': None,
-            'prove_runs': []
-        }
-        parameters_probe = get_updated(parameters, {
-            'vampire': {
-                'time_limit': self.time_limit_probe,
-                'mode': 'clausify'
-            }
-        })
-        probe_result = self.run_vampire_once(parameters_probe, os.path.join(outpath, 'probe'))
-        result['probe'] = self.result_data_relpath(probe_result, base_path)
-        if probe_result['output']['exit_code'] != 0:
+    def run_probe(self, outpath, parameters_probe, parameters_prove, n_prove_runs, executor, futures):
+        result = self.run_vampire_once(parameters_probe, outpath)
+        if result['output']['exit_code'] != 0:
             logging.warning('Probe run failed. Exit code: %s. Termination: %s'
-                            % (probe_result['output']['exit_code'], probe_result['output']['data']['termination']))
-            return result
-        for solve_index in range(parameters['run_count']):
-            parameters_run = get_updated(parameters, {
-                'vampire': {
-                    'time_limit': self.time_limit_solve,
-                    'random_seed': solve_index + 1
-                }
-            })
-            solve_result = self.run_vampire_once(parameters_run, os.path.join(outpath, str(solve_index)))
-            result['prove_runs'].append(self.result_data_relpath(solve_result, base_path))
+                            % (result['output']['exit_code'], result['output']['data']['termination']))
+        else:
+            for prove_run_index in range(n_prove_runs):
+                futures.add(executor.submit(self.run_prove, os.path.join(outpath, str(prove_run_index)),
+                                            get_updated(parameters_prove, {
+                                                'prove_run_index': prove_run_index,
+                                                'vampire': {
+                                                    'random_seed': prove_run_index + 1
+                                                }
+                                            })))
         return result
+
+    def run_prove(self, outpath, parameters):
+        return self.run_vampire_once(parameters, outpath)
 
     def run_vampire_once(self, parameters, outpath):
         parameters_run = get_updated(parameters, {
@@ -100,9 +115,3 @@ class BatchSolver:
         # The Vampire option --json_output requires the output directory to exist.
         os.makedirs(outpath, exist_ok=True)
         return self.vampire(parameters_run, outpath)
-
-    @staticmethod
-    def result_data_relpath(result, base_path):
-        result_base_path = result['parameters']['paths']['base']
-        result_data_path = result['parameters']['paths']['data']
-        return os.path.relpath(os.path.join(result_base_path, result_data_path), start=base_path)
