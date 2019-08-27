0da5023afec286649873d6d4bc42133c34905167
Filip Bartek <filip.bartek@hotmail.com>
2019-08-27 15:19:57 +0200

    Add environment setup script
    The script should be sourced to populate the environment with some
    useful values.

---
+++ env.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export VAMPIRE_ML=${VAMPIRE_ML:-$PWD}
+export VAMPIRE_ML_VENV=$VAMPIRE_ML/venv
+export VAMPIRE=$VAMPIRE_ML/vampire/vampire_rel
+
+export TPTP=${TPTP:-$HOME/TPTP-v7.2.0}
+export TPTP_PROBLEMS=$TPTP/Problems
