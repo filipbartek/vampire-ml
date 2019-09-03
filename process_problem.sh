a42c4d6965c0af9bff82acb4c864d206ec6e4653
Filip Bartek <filip.bartek@hotmail.com>
2019-09-03 12:03:36 +0200

    Print the Vampire command in process_problem.sh

--- process_problem.sh
+++ process_problem.sh
@@ -13,5 +13,7 @@ mkdir -p "$OUTPUT_DIR"
 env | sort > "$OUTPUT_DIR/env.txt"
 
 # TODO: Expose more Vampire options.
-time "$VAMPIRE" --include "$TPTP" --mode clausify --time_limit "${VAMPIRE_TIME_LIMIT:-10}" --json_output "$OUTPUT_DIR/vampire.json" "$TPTP_PROBLEMS/$PROBLEM" > "$OUTPUT_DIR/stdout.txt" 2> "$OUTPUT_DIR/stderr.txt"
+VAMPIRE_COMMAND=("$VAMPIRE" --include "$TPTP" --mode clausify --time_limit "${VAMPIRE_TIME_LIMIT:-10}" --json_output "$OUTPUT_DIR/vampire.json" "$TPTP_PROBLEMS/$PROBLEM")
+echo "${VAMPIRE_COMMAND[@]}" > "$OUTPUT_DIR/process.sh"
+time "${VAMPIRE_COMMAND[@]}" > "$OUTPUT_DIR/stdout.txt" 2> "$OUTPUT_DIR/stderr.txt"
 echo $? > "$OUTPUT_DIR/$EXITSTATUS_FILENAME"
