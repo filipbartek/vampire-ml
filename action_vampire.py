c1fd54d7f639d572b0dcf704f005efe0f775d1f6
Filip Bartek <filip.bartek@hotmail.com>
2019-09-04 16:26:09 +0200

    Revert "Do not save any per-batch files"
    Allow specifying output directory for batch output separately from the
    runs output. This allows cluster execution in multiple processes.
    
    This reverts commit f4ef9bc9cd5f66afc4f88fc0ca4bdd81827f37ce.

--- action_vampire.py
+++ action_vampire.py
@@ -2,12 +2,15 @@
 
 import collections
 import itertools
+import json
 import logging
+import os
 
 from tqdm import tqdm
 
 import file_path_list
 from batch import Batch
+from lazy_csv_writer import LazyCsvWriter
 
 default_vampire_options = {
     'probe': {
@@ -36,53 +39,106 @@ def compose_vampire_options(options_common, options_specific, name):
     return vampire_options
 
 
+def join_output_path(start, path):
+    if start is None and path is None:
+        raise RuntimeError('Both of the paths are null.')
+    if start is None:
+        return path
+    if path is None:
+        return start
+    return os.path.join(start, path)
+
+
 def call(namespace):
     vampire_options_probe = compose_vampire_options(namespace.vampire_options, namespace.vampire_options_probe, 'probe')
     vampire_options_solve = compose_vampire_options(namespace.vampire_options, namespace.vampire_options_solve, 'solve')
     problem_paths, problem_base_path = file_path_list.compose(namespace.problem_list, namespace.problem,
                                                               namespace.problem_base_path)
-    batch = Batch(namespace.vampire, vampire_options_probe, vampire_options_solve, namespace.output,
-                  namespace.solve_runs, namespace.jobs)
-    with tqdm(desc='Running Vampire', total=len(problem_paths), unit='problems') as t:
-        solve_runs = collections.Counter()
-        stats = {
-            'probe': {
-                'pass': 0,
-                'fail': 0,
-                'processed': 0,
-                'expected': len(problem_paths)
-            },
-            'solve': {
-                'pass': 0,
-                'fail': 0,
-                'skip': 0,
-                'processed': 0,
-                'expected': len(problem_paths) * namespace.solve_runs,
+    try:
+        output_batch = join_output_path(namespace.output, namespace.output_batch)
+    except RuntimeError:
+        logging.error('At least one of the options --output and --output_batch must be specified.')
+        return
+    try:
+        output_runs = join_output_path(namespace.output, namespace.output_runs)
+    except RuntimeError:
+        logging.error('At least one of the options --output and --output_runs must be specified.')
+        return
+
+    csv_file_path = os.path.join(output_batch, 'runs.csv')
+    problems_path = os.path.join(output_batch, 'problems.txt')
+    problems_successful_path = os.path.join(output_batch, 'problems_successful.txt')
+
+    os.makedirs(output_batch, exist_ok=True)
+    with open(os.path.join(output_batch, 'configuration.json'), 'w') as output_json_file:
+        json.dump({
+            'vampire': namespace.vampire,
+            'vampire_options_probe': vampire_options_probe,
+            'vampire_options_solve': vampire_options_solve,
+            'runs_per_problem': namespace.solve_runs,
+            'jobs': namespace.jobs,
+            'runs_csv': os.path.relpath(csv_file_path, output_batch),
+            'problem_base_path': problem_base_path,
+            'problems': os.path.relpath(problems_path, output_batch),
+            'problems_successful': os.path.relpath(problems_successful_path, output_batch)
+        }, output_json_file, indent=4)
+    with open(problems_path, 'w') as problems_file:
+        problems_file.write('\n'.join(problem_paths))
+        problems_file.write('\n')
+    batch = Batch(namespace.vampire, vampire_options_probe, vampire_options_solve, output_runs, namespace.solve_runs,
+                  namespace.jobs)
+    problems_successful = set()
+    with open(csv_file_path, 'w') as csv_file, open(problems_successful_path, 'w') as problems_successful_file:
+        csv_writer = LazyCsvWriter(csv_file)
+        with tqdm(desc='Running Vampire', total=len(problem_paths), unit='problems') as t:
+            solve_runs = collections.Counter()
+            stats = {
+                'probe': {
+                    'pass': 0,
+                    'fail': 0,
+                    'processed': 0,
+                    'expected': len(problem_paths)
+                },
+                'solve': {
+                    'pass': 0,
+                    'fail': 0,
+                    'skip': 0,
+                    'processed': 0,
+                    'expected': len(problem_paths) * namespace.solve_runs,
+                }
             }
-        }
-        t.set_postfix(stats)
-        for result in batch.generate_results(problem_paths, namespace.probe, problem_base_path):
-            if result['probe']:
-                stats['probe']['processed'] += 1
-                if result['exit_code'] == 0:
-                    stats['probe']['pass'] += 1
-                    if namespace.solve_runs == 0:
+            t.set_postfix(stats)
+            for result in batch.generate_results(problem_paths, namespace.probe, problem_base_path):
+                csv_writer.writerow({
+                    'output_path': os.path.relpath(result['paths']['output'], namespace.output),
+                    'problem_path': os.path.relpath(result['paths']['problem'], problem_base_path),
+                    'exit_code': result['exit_code'],
+                    'time_elapsed': result['time_elapsed']
+                })
+                if result['exit_code'] == 0 and result['paths']['problem'] not in problems_successful:
+                    problems_successful_file.write(result['paths']['problem'] + '\n')
+                    problems_successful.add(result['paths']['problem'])
+                if result['probe']:
+                    stats['probe']['processed'] += 1
+                    if result['exit_code'] == 0:
+                        stats['probe']['pass'] += 1
+                        if namespace.solve_runs == 0:
+                            t.update(1)
+                    else:
+                        stats['probe']['fail'] += 1
+                        stats['solve']['skip'] += namespace.solve_runs
+                        stats['solve']['processed'] += namespace.solve_runs
                         t.update(1)
                 else:
-                    stats['probe']['fail'] += 1
-                    stats['solve']['skip'] += namespace.solve_runs
-                    stats['solve']['processed'] += namespace.solve_runs
-                    t.update(1)
-            else:
-                solve_runs[result['paths']['problem']] += 1
-                stats['solve']['processed'] += 1
-                if solve_runs[result['paths']['problem']] == namespace.solve_runs:
-                    t.update(1)
-                if result['exit_code'] == 0:
-                    stats['solve']['pass'] += 1
-                else:
-                    stats['solve']['fail'] += 1
-            t.set_postfix(stats)
+                    solve_runs[result['paths']['problem']] += 1
+                    stats['solve']['processed'] += 1
+                    if solve_runs[result['paths']['problem']] == namespace.solve_runs:
+                        t.update(1)
+                    if result['exit_code'] == 0:
+                        stats['solve']['pass'] += 1
+                    else:
+                        stats['solve']['fail'] += 1
+                t.set_postfix(stats)
 
 
 def add_arguments(parser):
@@ -92,7 +148,9 @@ def add_arguments(parser):
                         help='input file with a list of problem paths')
     parser.add_argument('--problem_base_path', type=str,
                         help='the problem paths are relative to the base path')
-    parser.add_argument('--output', '-o', required=True, type=str, help='path to store the output files')
+    parser.add_argument('--output', '-o', type=str, help='main output directory')
+    parser.add_argument('--output_batch', type=str, help='directory to store the batch results')
+    parser.add_argument('--output_runs', type=str, help='directory to store the run results')
     # Output files: problems.txt, problems_successful.txt, runs.csv, result.json
     parser.add_argument('--vampire', type=str, default='vampire', help='Vampire command')
     parser.add_argument('--probe', action='store_true', help='probe each problem with a clausify run')
