c9c235a929635823ab824d666936db857416d74b
Filip Bartek <filip.bartek@hotmail.com>
2019-09-05 17:54:37 +0200

    Remove unused envvar EXITSTATUS_FILENAME

--- env.sh
+++ env.sh
@@ -17,5 +17,3 @@ if [ -n "${SLURM_JOB_ID-}" ]; then OUTPUT="slurm-$SLURM_JOB_ID"; fi
 if [ -n "${SLURM_ARRAY_JOB_ID-}" ]; then OUTPUT="slurm-$SLURM_ARRAY_JOB_ID"; fi
 : "${OUTPUT:=out/default}"
 export OUTPUT
-
-export EXITSTATUS_FILENAME=exitstatus.txt
