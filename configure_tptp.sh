d49b1cc2b49d39f44a8f74f46fa79899c473205b
Filip Bartek <filip.bartek@hotmail.com>
2019-09-09 11:26:36 +0200

    Add a script for downloading TPTP

---
+++ configure_tptp.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source env.sh
+
+TPTP_TGZ="$TPTP_VERSION.tgz"
+
+# http://www.tptp.org/
+wget "http://www.tptp.org/TPTP/Distribution/$TPTP_TGZ" -P /tmp
+tar -xzf "/tmp/$TPTP_TGZ" -C "$HOME"
+
+#find "$TPTP_PROBLEMS" -regextype sed -regex ".*/[A-Z]\{3\}[0-9]\{3\}[-+][1-9][0-9]*\(\.[0-9]\{3\}\)*\.p" -exec realpath --relative-to "$TPTP_PROBLEMS" {} + > problems_all.txt
