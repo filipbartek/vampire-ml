f522cbc9515e58eaf54bf464f57ec3085cdc4452
Filip Bartek <filip.bartek@hotmail.com>
2019-09-10 14:09:15 +0200

    LazyCsvWriter: Flush after each row
    and some cosmetic improvements.
    
    * Flush the output file every time a row is written.
    * Document `writerow`.
    * Reorder members.
    * Rename `_csv_file` to `_file`.
    * Collapse `__ensure_writer` into `writerow`.

--- lazy_csv_writer.py
+++ lazy_csv_writer.py
@@ -9,16 +9,22 @@ class LazyCsvWriter:
     A thread-safe csv.DictWriter that writes the header row automatically along with the first data row.
     """
     def __init__(self, file):
+        self._file = file
         self._lock = threading.Lock()
-        self._csv_file = file
         self._writer = None
 
     def writerow(self, row):
-        with self._lock:
-            self.__ensure_writer(row.keys())
-            self._writer.writerow(row)
+        """Write a CSV row and flush.
 
-    def __ensure_writer(self, fieldnames):
-        if self._writer is None:
-            self._writer = csv.DictWriter(self._csv_file, fieldnames=fieldnames)
-            self._writer.writeheader()
+        All the calls to this instance must use the same field names.
+
+        Mimics `csv.csvwriter.writerow`.
+
+        :param row: a dictionary that maps field names to field values
+        """
+        with self._lock:
+            if self._writer is None:
+                self._writer = csv.DictWriter(self._file, fieldnames=row.keys())
+                self._writer.writeheader()
+            self._writer.writerow(row)
+            self._file.flush()
