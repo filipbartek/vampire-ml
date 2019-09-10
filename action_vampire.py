58913edd8874b5bd914364dda4097b016c29c265
Filip Bartek <filip.bartek@hotmail.com>
2019-09-10 15:35:47 +0200

    Restructure `vampire` output for incremental runs
    Require separate runs for probe and solve.
    Disambiguate output directories by new options `--strategy-id` and
    `--job-id`.

--- action_vampire.py
+++ action_vampire.py
@@ -13,122 +13,93 @@ from batch import Batch
 from lazy_csv_writer import LazyCsvWriter
 
 default_vampire_options = {
-    'probe': {
-        '--encode': 'on',
-        '--time_limit': '10',
-        '--mode': 'clausify'
-    },
-    'solve': {
-        '--encode': 'on',
-        '--time_limit': '10',
-        '--statistics': 'full',
-        '--time_statistics': 'on',
-        '--proof': 'off',
-        '--symbol_precedence': 'scramble'
-    }
+    '--encode': 'on',
+    '--time_limit': '10',
+    '--statistics': 'full',
+    '--time_statistics': 'on',
+    '--proof': 'off',
+    '--symbol_precedence': 'scramble'
 }
 
 
-def compose_vampire_options(options_common, options_specific, name):
-    vampire_options = list(
-        itertools.chain(*(s.split() for s in options_common), *(s.split() for s in options_specific)))
-    for option, value in default_vampire_options[name].items():
+def compose_vampire_options(custom_options):
+    vampire_options = list(itertools.chain(*(s.split() for s in custom_options)))
+    for option, value in default_vampire_options.items():
         if option not in vampire_options:
-            logging.info(f'Defaulting {name}: {option} {value}')
+            logging.info(f'Defaulting Vampire option: {option} {value}')
             vampire_options.extend([option, value])
     return vampire_options
 
 
-def join_output_path(start, path):
-    if start is None and path is None:
-        raise RuntimeError('Both of the paths are null.')
-    if start is None:
-        return path
-    if path is None:
-        return start
-    return os.path.join(start, path)
-
-
 def call(namespace):
-    vampire_options_probe = compose_vampire_options(namespace.vampire_options, namespace.vampire_options_probe, 'probe')
-    vampire_options_solve = compose_vampire_options(namespace.vampire_options, namespace.vampire_options_solve, 'solve')
+    vampire_options = compose_vampire_options(namespace.vampire_options)
     problem_paths, problem_base_path = file_path_list.compose(namespace.problem_list, namespace.problem,
                                                               namespace.problem_base_path)
-    try:
-        output_batch = join_output_path(namespace.output, namespace.output_batch)
-    except RuntimeError:
-        logging.error('At least one of the options --output and --output_batch must be specified.')
-        return
-    try:
-        output_runs = join_output_path(namespace.output, namespace.output_runs)
-    except RuntimeError:
-        logging.error('At least one of the options --output and --output_runs must be specified.')
-        return
-    output_runs_relative = os.path.abspath(output_runs)
-    if (namespace.output_batch is None or not os.path.isabs(namespace.output_batch)) and (
-            namespace.output_runs is None or not os.path.isabs(namespace.output_runs)):
-        output_runs_relative = os.path.relpath(output_runs, output_batch)
+    assert namespace.output is not None
+    output_job = os.path.join(namespace.output, 'jobs')
+    output_problems = os.path.join(namespace.output, 'problems')
+    if namespace.strategy_id is not None:
+        output_job = os.path.join(output_job, namespace.strategy_id)
+    if namespace.job_id is not None:
+        output_job = os.path.join(output_job, namespace.job_id)
+    csv_file_path = os.path.join(output_job, 'runs.csv')
+    problems_path = os.path.join(output_job, 'problems.txt')
+    problems_successful_path = os.path.join(output_job, 'problems_successful.txt')
 
-    csv_file_path = os.path.join(output_batch, 'runs.csv')
-    problems_path = os.path.join(output_batch, 'problems.txt')
-    problems_successful_path = os.path.join(output_batch, 'problems_successful.txt')
-
-    os.makedirs(output_batch, exist_ok=True)
-    with open(os.path.join(output_batch, 'batch.json'), 'w') as output_json_file:
+    # We assume that `os.makedirs` is thread-safe.
+    os.makedirs(output_job, exist_ok=True)
+    with open(os.path.join(output_job, 'job.json'), 'w') as output_json_file:
+        # TODO: Improve output property names.
         json.dump({
-            'run_output_base_path': output_runs_relative,
-            'runs_csv': os.path.relpath(csv_file_path, output_batch),
+            'output': os.path.relpath(namespace.output, output_job),
+            'run_output_base_path': os.path.relpath(output_problems, output_job),
+            'runs_csv': os.path.relpath(csv_file_path, output_job),
             'problem_base_path': problem_base_path,
-            'problems': os.path.relpath(problems_path, output_batch),
-            'problems_successful': os.path.relpath(problems_successful_path, output_batch),
+            'problems': os.path.relpath(problems_path, output_job),
+            'problems_successful': os.path.relpath(problems_successful_path, output_job),
             'solve_runs_per_problem': namespace.solve_runs,
-            'jobs': namespace.jobs,
+            'cpus': namespace.cpus,
+            'strategy_id': namespace.strategy_id,
+            'job_id': namespace.job_id,
+            'scratch': namespace.scratch,
+            'cwd': os.getcwd(),
+            'no_clobber': namespace.no_clobber,
+            'vampire_timeout': namespace.vampire_timeout,
             'vampire': namespace.vampire,
-            'vampire_options_probe': vampire_options_probe,
-            'vampire_options_solve': vampire_options_solve
+            'vampire_options': vampire_options
         }, output_json_file, indent=4)
     with open(problems_path, 'w') as problems_file:
         problems_file.write('\n'.join(problem_paths))
         problems_file.write('\n')
-    batch = Batch(namespace.vampire, vampire_options_probe, vampire_options_solve, output_runs, namespace.solve_runs,
-                  namespace.timeout_probe, namespace.timeout_solve, namespace.jobs)
+    assert namespace.solve_runs >= 1
+    batch = Batch(namespace.vampire, vampire_options, output_problems, namespace.solve_runs, namespace.strategy_id,
+                  namespace.vampire_timeout, namespace.cpus)
     problems_successful = set()
     with open(csv_file_path, 'w') as csv_file, open(problems_successful_path, 'w') as problems_successful_file:
         csv_writer = LazyCsvWriter(csv_file)
-        with tqdm(desc='Running Vampire', total=len(problem_paths), unit='problems') as t:
-            solve_runs = collections.Counter()
+        with tqdm(desc='Running Vampire', total=len(problem_paths) * namespace.solve_runs, unit='problems') as t:
             stats = collections.Counter()
             t.set_postfix_str(stats)
-            for result in batch.generate_results(problem_paths, namespace.probe, problem_base_path):
+            for run_info in batch.generate_results(problem_paths, problem_base_path):
                 csv_writer.writerow({
-                    'output_path': os.path.relpath(result['paths']['output'], output_runs),
-                    'problem_path': os.path.relpath(result['paths']['problem'], problem_base_path),
-                    'probe': result['probe'],
-                    'timeout': result['timeout'],
-                    'exit_code': result['exit_code'],
-                    'time_elapsed': result['time_elapsed']
+                    'output_path': os.path.relpath(run_info['paths']['output'], output_problems),
+                    'problem_path': os.path.relpath(run_info['paths']['problem'], problem_base_path),
+                    'timeout': run_info['result']['timeout_expired'],
+                    'exit_code': run_info['result']['exit_code'],
+                    'time_elapsed': run_info['result']['time_elapsed']
                 })
-                if result['exit_code'] == 0 and result['paths']['problem'] not in problems_successful:
-                    problems_successful_file.write(result['paths']['problem'] + '\n')
-                    problems_successful.add(result['paths']['problem'])
-                if result['probe']:
-                    mode = 'probe'
+                if run_info['result']['exit_code'] == 0 and run_info['paths']['problem'] not in problems_successful:
+                    problems_successful_file.write(run_info['paths']['problem'] + '\n')
+                    problems_successful.add(run_info['paths']['problem'])
+                if run_info['result']['timeout_expired']:
+                    termination = 'timeout_expired'
+                    assert run_info['result']['exit_code'] is None
                 else:
-                    mode = 'solve'
-                if result['timeout']:
-                    termination = 'timeout'
-                    assert result['exit_code'] is None
-                else:
-                    assert result['exit_code'] is not None
-                    termination = result['exit_code']
-                stats[(mode, termination)] += 1
+                    assert run_info['result']['exit_code'] is not None
+                    termination = run_info['result']['exit_code']
+                stats[termination] += 1
                 t.set_postfix_str(stats)
-                if result['probe'] and (namespace.solve_runs == 0 or result['timeout'] or result['exit_code'] != 0):
-                    t.update(1)
-                if not result['probe']:
-                    solve_runs[result['paths']['problem']] += 1
-                    if solve_runs[result['paths']['problem']] == namespace.solve_runs:
-                        t.update(1)
+                t.update(1)
 
 
 def add_arguments(parser):
@@ -138,23 +109,23 @@ def add_arguments(parser):
                         help='input file with a list of problem paths')
     parser.add_argument('--problem-base-path', type=str,
                         help='the problem paths are relative to the base path')
-    parser.add_argument('--output', '-o', type=str, help='main output directory')
-    parser.add_argument('--output-batch', type=str, help='directory to store the batch results')
-    parser.add_argument('--output-runs', type=str, help='directory to store the run results')
-    # Output files: problems.txt, problems_successful.txt, runs.csv, result.json
-    parser.add_argument('--vampire', type=str, default='vampire', help='Vampire command')
-    parser.add_argument('--probe', action='store_true', help='probe each problem with a clausify run')
+    # Naming convention: `sbatch --output`
+    parser.add_argument('--output', '-o', required=True, type=str, help='main output directory')
+    parser.add_argument('--strategy-id',
+                        help='Identifier of strategy. Disambiguates job and problem run output directories.')
+    parser.add_argument('--job-id', help='Identifier of job. Disambiguates job output directory.')
+    # Naming convention: `sbatch --cpus-per-task`
+    parser.add_argument('--cpus', '-c', type=int, default=1, help='number of jobs to run in parallel')
+    # TODO: Expose finer control of the Vampire option `--random_seed`.
     parser.add_argument('--solve-runs', type=int, default=1,
-                        help='Number of solving Vampire executions per problem. Useful namely with `--vampire_options_solve \"--symbol_precedence scramble\"`.')
-    parser.add_argument('--timeout-probe', type=float, help='kill Vampire after this many seconds in probe runs')
-    parser.add_argument('--timeout-solve', type=float, help='kill Vampire after this many seconds in solve runs')
-    parser.add_argument('--jobs', '-j', type=int, default=1, help='number of jobs to run in parallel')
-
-    vampire_options = parser.add_argument_group('Vampire options',
-                                                'Options passed to Vampire. Run `vampire --show_options on --show_experimental_options on` to print the options supported by Vampire. Options automatically overriden: --random_seed, --json_output')
-    vampire_options.add_argument('--vampire-options', action='append', default=[],
-                                 help='Options for all runs. Recommended options: --include.')
-    vampire_options.add_argument('--vampire-options-probe', action='append', default=[],
-                                 help='Options for probe runs. Recommended options: --mode clausify, --time_limit.')
-    vampire_options.add_argument('--vampire-options-solve', action='append', default=[],
-                                 help='Options for solve runs. Recommended options: --time_limit, --symbol_precedence scramble.')
+                        help='Number of Vampire executions per problem. '
+                             'Each of the executions uses a different value of the Vampire option `--random_seed`. '
+                             'Useful namely with `--vampire_options \"--symbol_precedence scramble\"`.')
+    parser.add_argument('--vampire', type=str, default='vampire', help='Vampire command')
+    parser.add_argument('--vampire-options', action='append', default=[],
+                        help='Options passed to Vampire. '
+                             'Run `vampire --show_options on --show_experimental_options on` to print the options '
+                             'supported by Vampire. '
+                             'Options automatically overridden: --random_seed, --json_output. '
+                             'Recommended options: --time_limit, --mode, --symbol_precedence, --include.')
+    parser.add_argument('--vampire-timeout', type=float, help='kill Vampire after this many seconds')
--- batch.py
+++ batch.py
@@ -11,22 +11,23 @@ import utils
 
 
 class Batch:
-    def __init__(self, vampire, vampire_options_probe, vampire_options_solve, output_path, solve_runs_per_problem,
-                 timeout_probe=None, timeout_solve=None, jobs=1):
+    # TODO: Add support for `--no-clobber`.
+    # TODO: Add support for `--scratch`.
+    def __init__(self, vampire, vampire_options, output_path, solve_runs_per_problem, strategy_id, vampire_timeout=None, cpus=1):
         self._vampire = vampire
-        self._vampire_options_probe = vampire_options_probe
-        self._vampire_options_solve = vampire_options_solve
+        self._vampire_options = vampire_options
         assert output_path is not None
         self._output_path = output_path
+        assert solve_runs_per_problem >= 1
         self._solve_runs_per_problem = solve_runs_per_problem
-        self._timeout_probe = timeout_probe
-        self._timeout_solve = timeout_solve
-        assert jobs >= 1
-        self._jobs = jobs
-        self._executor = concurrent.futures.ThreadPoolExecutor(max_workers=jobs)
+        self._strategy_id = strategy_id
+        self._vampire_timeout = vampire_timeout
+        assert cpus >= 1
+        self._cpus = cpus
+        self._executor = concurrent.futures.ThreadPoolExecutor(max_workers=cpus)
         self._futures = set()
 
-    def generate_results(self, problem_paths, probe, problem_base_path=None):
+    def generate_results(self, problem_paths, problem_base_path=None):
         assert len(self._futures) == 0
         if len(problem_paths) == 0:
             logging.warning('No problems given.')
@@ -34,17 +35,19 @@ class Batch:
             problem_base_path = os.path.commonpath(problem_paths)
         try:
             for problem_path in problem_paths:
-                assert self._jobs >= 1
-                for result in self.reduce_futures(self._jobs - 1):
-                    yield result
                 problem_output_path = self._output_path
-                # TODO: Can this condition be omitted?
+                # If we omitted this condition, then a '.' might be added to the output path.
                 if problem_path != problem_base_path:
                     problem_output_path = os.path.join(self._output_path,
                                                        os.path.relpath(problem_path, problem_base_path))
-                self.__solve_one_problem_async(problem_output_path, problem_path, probe)
-            for result in self.reduce_futures():
-                yield result
+                if self._strategy_id is not None:
+                    problem_output_path = os.path.join(problem_output_path, self._strategy_id)
+                self.__solve_one_problem_async(problem_output_path, problem_path)
+            for future in concurrent.futures.as_completed(self._futures):
+                assert future.done()
+                if future.cancelled():
+                    continue
+                yield future.result()
         except KeyboardInterrupt:
             for future in self._futures:
                 future.cancel()
@@ -52,68 +55,70 @@ class Batch:
         finally:
             self._futures.clear()
 
-    def reduce_futures(self, n=0):
-        while len(self._futures) > n:
-            done, _ = concurrent.futures.wait(list(self._futures), return_when=concurrent.futures.FIRST_COMPLETED)
-            for future in done:
-                self._futures.remove(future)
-                assert future.done()
-                if future.cancelled():
-                    continue
-                yield future.result()
-
-    def __solve_one_problem_async(self, output_path, problem_path, probe):
-        assert self._solve_runs_per_problem >= 0
-        if probe:
-            self.__solve_one_run_async(os.path.join(output_path, 'probe'), output_path, problem_path, probe)
-            return
-        if self._solve_runs_per_problem == 0:
-            return
+    def __solve_one_problem_async(self, output_path, problem_path):
+        assert self._solve_runs_per_problem >= 1
         run_directory_name_width = len(str(self._solve_runs_per_problem - 1))
         for problem_run_index in range(self._solve_runs_per_problem):
-            run_output_path = os.path.join(output_path, str(problem_run_index).zfill(run_directory_name_width))
-            self.__solve_one_run_async(run_output_path, output_path, problem_path, probe, problem_run_index)
+            run_output_path = output_path
+            random_seed_zero_based = None
+            if self._solve_runs_per_problem > 1:
+                run_output_path = os.path.join(run_output_path, str(problem_run_index).zfill(run_directory_name_width))
+                random_seed_zero_based = problem_run_index
+            self.__solve_one_run_async(run_output_path, output_path, problem_path, random_seed_zero_based)
 
-    def __solve_one_run_async(self, output_path, problem_output_path, problem_path, probe, problem_run_index=None):
-        vampire_options = self.compose_vampire_options(problem_path, probe, problem_run_index)
+    def __solve_one_run_async(self, output_path, problem_output_path, problem_path, random_seed_zero_based=None):
+        vampire_options = self.compose_vampire_options(problem_path, random_seed_zero_based)
         future = self._executor.submit(self.__solve_one_run_sync, output_path, problem_output_path, vampire_options,
-                                       problem_path, probe)
-        future.probe = probe
-        if probe:
-            future.add_done_callback(self.probe_done_callback)
+                                       problem_path)
         self._futures.add(future)
 
-    def compose_vampire_options(self, problem_path, probe, random_seed_zero_based=None):
-        vampire_options = self._vampire_options_solve
-        if probe:
-            vampire_options = self._vampire_options_probe
-        vampire_options = vampire_options + [problem_path]
+    def compose_vampire_options(self, problem_path, random_seed_zero_based=None):
+        vampire_options = self._vampire_options.copy()
+        vampire_options.extend([problem_path])
         if random_seed_zero_based is not None:
             if '--random_seed' in vampire_options:
                 logging.warning('Overriding --random_seed.')
-            vampire_options = vampire_options + ['--random_seed', str(random_seed_zero_based + 1)]
+            vampire_options.extend(['--random_seed', str(random_seed_zero_based + 1)])
         return vampire_options
 
-    def __solve_one_run_sync(self, output_path, problem_output_path, vampire_options, problem_path, probe):
+    def __solve_one_run_sync(self, output_path, problem_output_path, vampire_options, problem_path):
+        # TODO: Move to `compose_vampire_options`.
+        # TODO: Add support for scratch space.
         json_output_path = os.path.join(output_path, 'vampire.json')
         if '--json_output' in vampire_options:
             logging.warning('Overriding --json_output.')
         vampire_options = vampire_options + ['--json_output', json_output_path]
         vampire_args = [self._vampire] + vampire_options
         complete_command = ' '.join(vampire_args)
-        if probe:
-            timeout_seconds = self._timeout_probe
-        else:
-            timeout_seconds = self._timeout_solve
+        timeout_seconds = self._vampire_timeout
         if timeout_seconds is None:
             vampire_time_limit = utils.option_value(vampire_options, '--time_limit')
             if vampire_time_limit is not None:
                 # TODO: Support all the time formats supported by Vampire.
-                vampire_time_limit_seconds = int(vampire_time_limit)
+                vampire_time_limit_seconds = float(vampire_time_limit)
                 timeout_seconds = vampire_time_limit_seconds + 1
-        exit_status = None
+        # TODO: Add path to `job.json`.
+        paths = {
+            'cwd': os.getcwd(),
+            'result': 'result.json',
+            'problem': problem_path,
+            'problem_output': problem_output_path,
+            'output': output_path,
+            'stdout': 'stdout.txt',
+            'stderr': 'stderr.txt',
+            'vampire_json': 'vampire.json',
+            'configuration': 'configuration.json'
+        }
+        configuration = {
+            'command': complete_command,
+            'timeout_seconds': timeout_seconds,
+            'vampire': self._vampire,
+            'options': vampire_options
+        }
         # If JSON output directory does not exist, Vampire fails.
         os.makedirs(output_path, exist_ok=True)
+        with open(os.path.join(output_path, 'configuration.json'), 'w') as configuration_json:
+            json.dump({'paths': paths, 'configuration': configuration}, configuration_json, indent=4)
         time_start = time.time()
         try:
             cp = subprocess.run(vampire_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=timeout_seconds,
@@ -126,55 +131,30 @@ class Batch:
         except subprocess.TimeoutExpired as e:
             time_elapsed = time.time() - time_start
             result_timeout = True
+            exit_status = None
             stdout_str = e.stdout
             stderr_str = e.stderr
+        result = {
+            'timeout_expired': result_timeout,
+            'exit_code': exit_status,
+            'time_elapsed': time_elapsed
+        }
+        with open(os.path.join(output_path, paths['result']), 'w') as run_json:
+            json.dump({'paths': paths, 'result': result}, run_json, indent=4)
+        with open(os.path.join(output_path, paths['stdout']), 'w') as stdout_file:
+            stdout_file.write(stdout_str)
+        with open(os.path.join(output_path, paths['stderr']), 'w') as stderr_file:
+            stderr_file.write(stderr_str)
         # The JSON files may take a lot of space.
         if result_timeout or exit_status != 0:
             try:
                 os.remove(json_output_path)
             except FileNotFoundError:
                 pass
-        result_thin = {
-            'cwd': os.getcwd(),
-            'command': complete_command,
-            'timeout': result_timeout,
-            'exit_code': exit_status,
-            'time_elapsed': time_elapsed,
-            'vampire': self._vampire,
-            'probe': probe,
-            'solve_runs': self._solve_runs_per_problem,
-            'paths': {
-                'problem': problem_path,
-                'problem_output': problem_output_path,
-                'output': output_path,
-                'stdout': 'stdout.txt',
-                'stderr': 'stderr.txt',
-                'vampire_json': 'vampire.json'
-            },
-            'options': vampire_options
-        }
-        result_complete = result_thin.copy()
-        result_complete.update({
+        return {
+            'paths': paths,
+            'configuration': configuration,
+            'result': result,
             'stdout': stdout_str,
             'stderr': stderr_str
-        })
-        result_complete['paths']['run_json'] = 'run.json'
-        # TODO: Consider delegating the writing to the caller.
-        with open(os.path.join(output_path, result_thin['paths']['stdout']), 'w') as stdout_file:
-            stdout_file.write(stdout_str)
-        with open(os.path.join(output_path, result_thin['paths']['stderr']), 'w') as stderr_file:
-            stderr_file.write(stderr_str)
-        with open(os.path.join(output_path, result_complete['paths']['run_json']), 'w') as run_json:
-            json.dump(result_thin, run_json, indent=4)
-        return result_complete
-
-    def probe_done_callback(self, future):
-        assert future.probe
-        assert future.done()
-        if future.cancelled():
-            return
-        result = future.result()
-        assert result['probe']
-        if result['exit_code'] == 0:
-            # Schedule the solve runs.
-            self.__solve_one_problem_async(result['paths']['problem_output'], result['paths']['problem'], False)
+        }
