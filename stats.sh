69f436877901ee11be2b23b2f80a498bbe1ab1ee
Filip Bartek <filip.bartek@hotmail.com>
2019-09-13 18:59:49 +0200

    Adjust stats.sh to the new interface of stats

--- stats.sh
+++ stats.sh
@@ -6,9 +6,7 @@ source env.sh
 
 RESULTS_BASE=${RESULTS_BASE:-out/default}
 STRATEGY_ID=${STRATEGY_ID:-probe}
-# JOB_ID may be a glob pattern.
-JOB_ID=${JOB_ID:-*/*}
-RESULTS=${RESULTS:-$RESULTS_BASE/jobs/$STRATEGY_ID/$JOB_ID/job.json}
+RESULTS=${RESULTS:-$RESULTS_BASE/problems/**/$STRATEGY_ID/**/result.json}
 OUTPUT_ID=${OUTPUT_ID:-$STRATEGY_ID}
 OUTPUT=${OUTPUT:-$RESULTS_BASE/stats/$OUTPUT_ID}
 PROBLEMS=${PROBLEMS:-problems_cnf_fof.txt}
