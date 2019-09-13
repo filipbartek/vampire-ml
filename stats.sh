172b2f876481e5ffd5275d4802dccde233b24c99
Filip Bartek <filip.bartek@hotmail.com>
2019-09-13 12:43:50 +0200

    stats.sh: Run Python in optimized mode

--- stats.sh
+++ stats.sh
@@ -6,4 +6,4 @@ OUTPUT=${OUTPUT:-out/default}
 STRATEGY_ID=${STRATEGY_ID:-probe}
 PROBLEMS=${PROBLEMS:-problems_cnf_fof.txt}
 
-./vampire-ml.py stats "$OUTPUT/jobs/$STRATEGY_ID/*/job.json" --output "$OUTPUT/stats/$STRATEGY_ID" --problem_list "$PROBLEMS" "$@"
+python -O vampire-ml.py stats "$OUTPUT/jobs/$STRATEGY_ID/*/job.json" --output "$OUTPUT/stats/$STRATEGY_ID" --problem-list "$PROBLEMS" "$@"
