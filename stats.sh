7e439e252c2d61fbe71e891e5aba3fc1288621b4
Filip Bartek <filip.bartek@hotmail.com>
2019-09-13 12:56:11 +0200

    Allow more control in stats.sh

--- stats.sh
+++ stats.sh
@@ -4,8 +4,11 @@ set -euo pipefail
 
 source env.sh
 
-OUTPUT=${OUTPUT:-out/default}
+RESULTS_BASE=${RESULTS_BASE:-out/default}
 STRATEGY_ID=${STRATEGY_ID:-probe}
+RESULTS=${RESULTS:-$RESULTS_BASE/jobs/$STRATEGY_ID/*/job.json}
+OUTPUT_ID=${OUTPUT_ID:-$STRATEGY_ID}
+OUTPUT=${OUTPUT:-$RESULTS_BASE/stats/$OUTPUT_ID}
 PROBLEMS=${PROBLEMS:-problems_cnf_fof.txt}
 
-python -O vampire-ml.py stats "$OUTPUT/jobs/$STRATEGY_ID/*/job.json" --output "$OUTPUT/stats/$STRATEGY_ID" --problem-list "$PROBLEMS" "$@"
+python -O vampire-ml.py stats "$RESULTS" --output "$OUTPUT" --problem-list "$PROBLEMS" "$@"
