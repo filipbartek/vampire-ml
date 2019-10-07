67a4799f72af69a7519ca1c973bb0076786bae65
Filip Bartek <filip.bartek@hotmail.com>
2019-10-07 14:31:50 +0200

    Expose the whole sbatch --array parameter

--- vampire-array.sh
+++ vampire-array.sh
@@ -9,6 +9,7 @@ CPUS_PER_TASK=${CPUS_PER_TASK:-1}
 TIME_PER_TASK=${TIME_PER_TASK:-68}
 PARTITION=${PARTITION:-compute,gpu}
 ARRAY_TASK_COUNT=${ARRAY_TASK_COUNT:-1000}
+ARRAY=${ARRAY:-0-$((ARRAY_TASK_COUNT - 1))}
 
 mkdir -p "${OUTPUT:-out}/slurm"
-sbatch --input="$PROBLEMS" --cpus-per-task="$CPUS_PER_TASK" --time="$TIME_PER_TASK" --partition="$PARTITION" --array=0-$((ARRAY_TASK_COUNT - 1)) --mail-type=FAIL,REQUEUE,STAGE_OUT --job-name="${STRATEGY_ID:-vampire}" --comment="$(git rev-parse --verify HEAD)" --output="${OUTPUT:-out}/slurm/%A_%a.out" vampire.sh "$@"
+sbatch --input="$PROBLEMS" --cpus-per-task="$CPUS_PER_TASK" --time="$TIME_PER_TASK" --partition="$PARTITION" --array="$ARRAY" --mail-type=FAIL,REQUEUE,STAGE_OUT --job-name="${STRATEGY_ID:-vampire}" --comment="$(git rev-parse --verify HEAD)" --output="${OUTPUT:-out}/slurm/%A_%a.out" vampire.sh "$@"
