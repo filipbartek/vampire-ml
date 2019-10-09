1f4f29157d4d5e5eab9a1120623de2d6dacc8a02
Filip Bartek <filip.bartek@hotmail.com>
2019-10-09 11:35:52 +0200

    Allow overriding vampire executable

--- env.sh
+++ env.sh
@@ -3,9 +3,8 @@
 # Source this file in bash to populate the environment with useful variables and activate the venv.
 
 export VAMPIRE_ML=${VAMPIRE_ML:-$PWD}
-export VAMPIRE_ML_VENV=$VAMPIRE_ML/venv
-export VAMPIRE_DIR=$VAMPIRE_ML/vampire
-export VAMPIRE=$VAMPIRE_DIR/vampire_rel
+export VAMPIRE_ML_VENV=${VAMPIRE_ML_VENV:-$VAMPIRE_ML/venv}
+export VAMPIRE=${VAMPIRE:-$VAMPIRE_ML/vampire/vampire_rel}
 
 export TPTP_VERSION=${TPTP_VERSION:-TPTP-v7.3.0}
 export TPTP=${TPTP:-$HOME/$TPTP_VERSION}
