37c672589ad847dbeeb1b232d48933efc7b702b3
Filip Bartek <filip.bartek@hotmail.com>
2019-10-09 13:45:56 +0200

    vampire-array-*.sh: Allow delegating some config
    to the called scripts by setting the vars to null.

--- vampire-array-probe.sh
+++ vampire-array-probe.sh
@@ -2,10 +2,10 @@
 
 set -euo pipefail
 
-export OUTPUT=${OUTPUT:-out/default}
-export STRATEGY_ID=${STRATEGY_ID:-probe}
+export OUTPUT=${OUTPUT-out/default}
+export STRATEGY_ID=${STRATEGY_ID-probe}
 export VAMPIRE_MODE=clausify
-export CPUS_PER_TASK=${CPUS_PER_TASK:-4}
-export TIME_PER_TASK=${TIME_PER_TASK:-8}
+export CPUS_PER_TASK=${CPUS_PER_TASK-4}
+export TIME_PER_TASK=${TIME_PER_TASK-8}
 
 ./vampire-array.sh "$@"
