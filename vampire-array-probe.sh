a122df49a4ac4fbda5b86b876a62d9980368182d
Filip Bartek <filip.bartek@hotmail.com>
2019-10-11 16:58:32 +0200

    Use only 1 CPU per task by default
    I assume this way I can utilize more CPUs in the cluster since each job
    will now require only 1 CPU (rather than 4 or 16) and only 2 GB RAM
    (2 GB per CPU). Since I typically run an array of 1000 jobs and there is
    less than 1000 CPUs in the cluster, this change is likely to make the
    execution faster.

--- vampire-array-probe.sh
+++ vampire-array-probe.sh
@@ -5,7 +5,6 @@ set -euo pipefail
 export STRATEGY_ID=${STRATEGY_ID-probe}
 export OUTPUT=${OUTPUT-out/default/$STRATEGY_ID}
 export VAMPIRE_MODE=clausify
-export CPUS_PER_TASK=${CPUS_PER_TASK-4}
 export TIME_PER_TASK=${TIME_PER_TASK-8}
 
 ./vampire-array.sh "$@"
--- vampire-array-scramble.sh
+++ vampire-array-scramble.sh
@@ -5,8 +5,7 @@ set -euo pipefail
 export STRATEGY_ID=${STRATEGY_ID-scramble}
 export OUTPUT=${OUTPUT-out/default/$STRATEGY_ID}
 export VAMPIRE_MODE=vampire
-export CPUS_PER_TASK=${CPUS_PER_TASK-16}
-export TIME_PER_TASK=${TIME_PER_TASK-8}
+export TIME_PER_TASK=${TIME_PER_TASK-60}
 export VAMPIRE_SYMBOL_PRECEDENCE=scramble
 export SOLVE_RUNS_PER_PROBLEM=${SOLVE_RUNS_PER_PROBLEM-16}
 
