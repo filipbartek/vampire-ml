6bb5f1a22c6dac430275c928bc979a5602f0c79b
Filip Bartek <filip.bartek@hotmail.com>
2019-10-14 14:59:57 +0200

    Set a sbatch mem limit consistent with Vampire limit

--- vampire-array.sh
+++ vampire-array.sh
@@ -5,6 +5,7 @@
 set -euo pipefail
 
 PROBLEMS=${PROBLEMS:-problems_cnf_fof.txt}
+VAMPIRE_MEMORY_LIMIT=${VAMPIRE_MEMORY_LIMIT:-8192}
 CPUS_PER_TASK=${CPUS_PER_TASK:-1}
 TIME_PER_TASK=${TIME_PER_TASK:-60}
 PARTITION=${PARTITION:-compute}
@@ -12,4 +13,4 @@ ARRAY_TASK_COUNT=${ARRAY_TASK_COUNT:-1000}
 ARRAY=${ARRAY:-0-$((ARRAY_TASK_COUNT - 1))}
 
 mkdir -p "${OUTPUT:-out}/slurm"
-sbatch --input="$PROBLEMS" --cpus-per-task="$CPUS_PER_TASK" --time="$TIME_PER_TASK" --partition="$PARTITION" --array="$ARRAY" --mail-type=FAIL,REQUEUE,STAGE_OUT --job-name="${STRATEGY_ID:-vampire}" --comment="$(git rev-parse --verify HEAD)" --output="${OUTPUT:-out}/slurm/%A_%a.out" vampire.sh "$@"
+sbatch --input="$PROBLEMS" --cpus-per-task="$CPUS_PER_TASK" --time="$TIME_PER_TASK" --mem-per-cpu=$((VAMPIRE_MEMORY_LIMIT + 128)) --partition="$PARTITION" --array="$ARRAY" --mail-type=FAIL,REQUEUE,STAGE_OUT --job-name="${STRATEGY_ID:-vampire}" --comment="$(git rev-parse --verify HEAD)" --output="${OUTPUT:-out}/slurm/%A_%a.out" vampire.sh "$@"
