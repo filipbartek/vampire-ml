e1777ff5c98ec47cfb9404a42aa4cd89879d913e
Filip Bartek <filip.bartek@hotmail.com>
2019-11-15 14:26:34 +0100

    all.sh: Integrate probe and scramble results
    and use the default time limit in large stats runs.

--- all.sh
+++ all.sh
@@ -7,8 +7,9 @@ WORKSPACE=${WORKSPACE:-out/default}
 WORKSPACE_PROBE=$WORKSPACE/probe
 PROBE_JOB_ID=$(OUTPUT=$WORKSPACE_PROBE ./vampire-array-probe.sh)
 RESULTS_BASE=$WORKSPACE_PROBE OUTPUT_ID=minimal sbatch --time=10 --job-name=stats-probe-minimal --dependency=afterok:"$PROBE_JOB_ID" stats.sh
-RESULTS_BASE=$WORKSPACE_PROBE OUTPUT_ID=full sbatch --time=240 --job-name=stats-probe-full --dependency=afterok:"$PROBE_JOB_ID" stats.sh --source stdout symbols clauses
+PROBE_STATS_JOB_ID=$(RESULTS_BASE=$WORKSPACE_PROBE OUTPUT_ID=full sbatch --job-name=stats-probe-full --dependency=afterok:"$PROBE_JOB_ID" stats.sh --source stdout symbols clauses)
 
 WORKSPACE_SCRAMBLE=$WORKSPACE/scramble
 SCRAMBLE_JOB_ID=$(OUTPUT=$WORKSPACE_SCRAMBLE ./vampire-array-scramble.sh)
-RESULTS_BASE=$WORKSPACE_SCRAMBLE OUTPUT_ID=stdout sbatch --time=240 --job-name=stats-scramble --dependency=afterok:"$SCRAMBLE_JOB_ID" stats.sh --source stdout
+RESULTS_BASE=$WORKSPACE_SCRAMBLE OUTPUT_ID=stdout sbatch --job-name=stats-scramble-stdout --dependency=afterok:"$SCRAMBLE_JOB_ID" stats.sh --source stdout
+RESULTS_BASE=$WORKSPACE_SCRAMBLE OUTPUT_ID=complete sbatch --job-name=stats-scramble-complete --dependency=afterok:"$SCRAMBLE_JOB_ID":"$PROBE_STATS_JOB_ID" stats.sh --source stdout --input-probe-runs-pickle "$WORKSPACE_PROBE/stats/full/runs.pkl"
