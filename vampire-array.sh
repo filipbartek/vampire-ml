a4c939e8fc16b20d353b5f42222b44643335da56
Filip Bartek <filip.bartek@hotmail.com>
2019-11-21 14:23:47 +0100

    vampire-array.sh: Finish by aggregating the stats

--- vampire-array.sh
+++ vampire-array.sh
@@ -11,11 +11,20 @@ export VAMPIRE_MEMORY_LIMIT=${VAMPIRE_MEMORY_LIMIT:-8192}
 OUTPUT_SLURM=${OUTPUT_SLURM:-$OUTPUT/slurm}
 PROBLEMS=${PROBLEMS:-problems_cnf_fof.txt}
 CPUS_PER_TASK=${CPUS_PER_TASK:-1}
-TIME_PER_TASK=${TIME_PER_TASK:-60}
 PARTITION=${PARTITION:-compute}
 ARRAY_TASK_COUNT=${ARRAY_TASK_COUNT:-1000}
 ARRAY=${ARRAY:-0-$((ARRAY_TASK_COUNT - 1))}
-JOB_NAME=${JOB_NAME:-vampire:$OUTPUT}
+TIME_PER_TASK=${TIME_PER_TASK:-$((16748 * (SOLVE_RUNS_PER_PROBLEM + 1) * (VAMPIRE_TIME_LIMIT + 10) / (ARRAY_TASK_COUNT * 60)))}
+
+COMMON_SBATCH_OPTIONS=(
+  "--partition=$PARTITION"
+  "--cpus-per-task=$CPUS_PER_TASK"
+  "--mail-type=FAIL,REQUEUE,STAGE_OUT"
+  "--comment=$(git rev-parse --verify HEAD)"
+)
+
+echo "TIME_PER_TASK=$TIME_PER_TASK"
 
 mkdir -p "$OUTPUT_SLURM"
-sbatch --parsable --input="$PROBLEMS" --cpus-per-task="$CPUS_PER_TASK" --time="$TIME_PER_TASK" --mem-per-cpu=$((VAMPIRE_MEMORY_LIMIT + 128)) --partition="$PARTITION" --array="$ARRAY" --mail-type=FAIL,REQUEUE,STAGE_OUT --job-name="$JOB_NAME" --comment="$(git rev-parse --verify HEAD)" --output="$OUTPUT_SLURM/%A_%a.out" vampire.sh "$@"
+ARRAY_JOB_ID=$(sbatch "${COMMON_SBATCH_OPTIONS[@]}" --job-name="$OUTPUT:vampire" --output="$OUTPUT_SLURM/%A_%a.out" --parsable --input="$PROBLEMS" --time="$TIME_PER_TASK" --mem-per-cpu=$((VAMPIRE_MEMORY_LIMIT + 128)) --array="$ARRAY" vampire.sh "$@")
+sbatch "${COMMON_SBATCH_OPTIONS[@]}" --job-name="$OUTPUT:aggregate" --output="$OUTPUT_SLURM/%j.out" --dependency=afterok:"$ARRAY_JOB_ID" aggregate.sh
