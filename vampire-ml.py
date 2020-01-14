5f142843e112fe406fb5e9d68f24b1a831d54c73
Filip Bartek <filip.bartek@hotmail.com>
2020-01-14 14:02:55 +0100

    Disable debug log messages from matplotlib

--- vampire-ml.py
+++ vampire-ml.py
@@ -33,5 +33,6 @@ if __name__ == '__main__':
 
     if namespace.log is not None:
         logging.basicConfig(level=namespace.log)
+    logging.getLogger('matplotlib').setLevel(logging.INFO)
 
     namespace.action(namespace)
