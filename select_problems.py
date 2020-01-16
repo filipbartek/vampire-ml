ce228c5ca7238aa77db87e696a1dbfab1c22ea6b
Filip Bartek <filip.bartek@hotmail.com>
2020-01-16 15:06:04 +0100

    Add script select_problems.py

---
+++ select_problems.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3.7
+
+import logging
+
+import numpy as np
+import pandas as pd
+
+from results import save_df
+
+if __name__ == '__main__':
+    np.seterr(all='raise')
+    logging.basicConfig(level=logging.INFO)
+
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', help='Problems dataframe pickle')
+    parser.add_argument('--output-dir')
+    parser.add_argument('--n', default=100, type=int, help='Maximum number of problems to output')
+    parser.add_argument('--saturation-iterations-nunique-min', default=12, type=int)
+    namespace = parser.parse_args()
+
+    # Load
+    problems = pd.read_pickle(namespace.input)
+
+    # Select and sort
+    problems_chosen = problems[
+        problems[('saturation_iterations', 'nunique')] >= namespace.saturation_iterations_nunique_min]
+    problems_chosen = problems_chosen.sort_values(('score', 'std'), ascending=False)
+    problems_chosen = problems_chosen[:namespace.n]
+
+    # Save
+    save_df(problems_chosen, 'problems_selected', namespace.output_dir)
+    problems_chosen.index.to_series().to_csv('problems_selected.txt', index=False, header=False)
