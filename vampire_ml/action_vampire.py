81ec27c32e35ae146e93e4ec68b972974f730c64
Filip Bartek <filip.bartek@hotmail.com>
2020-03-07 11:31:14 +0100

    Implement pair scoring using linear regression and lasso

--- vampire_ml/action_vampire.py
+++ vampire_ml/action_vampire.py
@@ -11,6 +11,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy
 import seaborn as sns
+import sklearn
+import sklearn.impute
+import sklearn.linear_model
+import sklearn.pipeline
+import sklearn.preprocessing
 import yaml
 
 import vampyre
@@ -18,6 +23,7 @@ from utils import file_path_list
 from utils import makedirs_open
 from . import precedence
 from . import results
+from .sklearn_extensions import MeanRegression, QuantileImputer, Flattener
 
 
 def add_arguments(parser):
@@ -66,27 +72,45 @@ def execution_base_score(execution):
     return np.nan
 
 
-def generate_scores(executions, log_scale=False, normalize=False):
-    scores = map(lambda execution: execution_base_score(execution), executions)
-    scores = np.asarray(list(scores))
+def pairs_hit(precedences):
+    precedences = np.asarray(precedences)
+    m = precedences.shape[0]
+    n = precedences.shape[1]
+    assert precedences.shape == (m, n)
+    res = np.empty((m, n, n), np.bool)
+    precedence_inverse = np.empty(n, precedences.dtype)
+    for i, precedence in enumerate(precedences):
+        # https://stackoverflow.com/a/25535723/4054250
+        precedence_inverse[precedence] = np.arange(n)
+        res[i] = np.tri(n, k=-1, dtype=np.bool).transpose()[precedence_inverse, :][:, precedence_inverse]
+    return res
+
+
+def preprocess_scores(scores_base, failure_penalty_quantile, failure_penalty_factor, log_scale, normalize):
+    scores = scores_base.copy()
+    y_pipeline_steps = list()
+    quantile_imputer = None
+    if failure_penalty_quantile is not None:
+        quantile_imputer = QuantileImputer(copy=False, quantile=failure_penalty_quantile, factor=failure_penalty_factor)
+        y_pipeline_steps.append(('quantile', quantile_imputer))
     if log_scale:
-        scores = np.log(scores)
+        y_pipeline_steps.append(('log', sklearn.preprocessing.FunctionTransformer(func=np.log)))
     if normalize:
-        scores = (scores - np.nanmean(scores)) / np.nanstd(scores)
+        y_pipeline_steps.append(('normalize', sklearn.preprocessing.StandardScaler(copy=False)))
+    y_pipeline_steps.append(('passthrough', 'passthrough'))
+    # TODO: Expose the y pipeline so that it can be reused for prediction.
+    y_pipeline = sklearn.pipeline.Pipeline(y_pipeline_steps)
+    scores = y_pipeline.fit_transform(scores)[:, 0]
+    json.dump({
+        'count': len(scores),
+        'nan_count': np.count_nonzero(np.isnan(scores)),
+        'mean': np.nanmean(scores),
+        'std': np.nanstd(scores),
+        'max': np.nanmax(scores)
+    }, sys.stderr, indent=4)
+    if quantile_imputer is not None:
+        logging.info({'failure_score': quantile_imputer.fill_value})
     return scores
-
-
-def learn_ltot_general(executions, problem=None, output_dir=None, log_scale=False, normalize=False, failure_score=None):
-    scores = generate_scores(executions, log_scale=log_scale, normalize=normalize)
-    good_precedences = dict()
-    for precedence_option in executions[0].configuration.precedences.keys():
-        precedence_scores = zip((execution.configuration.precedences[precedence_option] for execution in executions),
-                                scores)
-        symbol_type = {'predicate_precedence': 'predicate', 'function_precedence': 'function'}[precedence_option]
-        good_precedences[precedence_option] = precedence.learn_precedence_lex(precedence_scores, symbol_type, problem,
-                                                                              output_dir=output_dir,
-                                                                              failure_score=failure_score)
-    return good_precedences
 
 
 def call(namespace):
@@ -178,37 +202,89 @@ def call(namespace):
                     logging.info(
                         f'Precedence learning skipped because signature is too large. Predicates: {len(problem.get_predicates())}. Functions: {len(problem.get_functions())}. Maximum: {namespace.learn_max_symbols}.')
                 else:
-                    params = itertools.product([False, True], ['ignore', 'dominate', 'mean', 'median', 'max'],
-                                               [None, 1, 2, 10, 100])
-                    for log_scale, failure_base, failure_factor in params:
-                        if (failure_base in ['ignore', 'dominate']) == (failure_factor is not None):
-                            continue
-                        name = f'ltot_general_{log_scale}_{failure_base}_{failure_factor}'
-                        if failure_base in ['ignore', 'dominate']:
-                            assert failure_factor is None
-                            failure_score = failure_base
-                        else:
-                            assert failure_base in ['mean', 'median', 'max']
-                            failure_base_value = None
-                            if failure_base == 'mean':
-                                failure_base_value = np.mean(saturation_iterations)
-                            if failure_base == 'median':
-                                failure_base_value = np.median(saturation_iterations)
-                            if failure_base == 'max':
-                                failure_base_value = np.max(saturation_iterations)
-                            assert failure_base_value is not None
-                            assert failure_factor is not None
-                            failure_score = failure_base_value * failure_factor
-                        try:
-                            precedences = learn_ltot_general(executions, problem=problem,
-                                                             output_dir=os.path.join(output_batch, name),
-                                                             log_scale=log_scale,
-                                                             failure_score=failure_score)
-                        except IndexError:
-                            logging.debug('Learning failed.', exc_info=True)
-                            continue
-                        # TODO: Try to improve the permutation by two-point swaps (hillclimbing).
-                        execution = problem.get_execution(precedences=precedences)
+                    scores_base = np.fromiter(map(execution_base_score, executions), dtype=np.float,
+                                              count=len(executions)).reshape(-1, 1)
+                    json.dump({
+                        'count': len(scores_base),
+                        'nan_count': np.count_nonzero(np.isnan(scores_base)),
+                        'mean': np.nanmean(scores_base),
+                        'std': np.nanstd(scores_base),
+                        'max': np.nanmax(scores_base)
+                    }, sys.stderr, indent=4)
+                    symbol_type_preprocessed_data = dict()
+                    for precedence_option in executions[0].configuration.precedences.keys():
+                        precedences = [execution.configuration.precedences[precedence_option] for execution in
+                                       executions]
+                        flattener = Flattener()
+                        pipeline = sklearn.pipeline.Pipeline([
+                            ('pairs_hit', sklearn.preprocessing.FunctionTransformer(func=pairs_hit)),
+                            ('flatten', flattener)
+                        ])
+                        x = pipeline.fit_transform(precedences)
+                        symbol_type_preprocessed_data[precedence_option] = {
+                            'x': x,
+                            'flattener': flattener
+                        }
+                    params = itertools.chain(
+                        itertools.product([False, True], [True], [1], [1, 2, 10],
+                                          ['mean', 'linear_regression', 'lasso_cv'], [1.0]),
+                        itertools.product([False, True], [True], [1], [1, 2, 10],
+                                          ['lasso'], [0.01, 0.1, 0.2, 1.0])
+                    )
+                    for log_scale, normalize, failure_penalty_quantile, failure_penalty_factor, pair_scoring, alpha in params:
+                        name = f'ltot_{log_scale}_{normalize}_{failure_penalty_quantile}_{failure_penalty_factor}_{pair_scoring}_{alpha}'
+                        # TODO: Fit score preprocessing only on the training set.
+                        y = preprocess_scores(scores_base, failure_penalty_quantile, failure_penalty_factor,
+                                              log_scale, normalize)
+                        good_precedences = dict()
+                        stats = dict()
+                        for precedence_option, data in symbol_type_preprocessed_data.items():
+                            x = data['x']
+                            flattener = data['flattener']
+                            # TODO: Disable copying of x in transformers.
+                            reg = {
+                                'mean': MeanRegression(),
+                                'linear_regression': sklearn.linear_model.LinearRegression(copy_X=False),
+                                'lasso': sklearn.linear_model.Lasso(alpha=alpha, copy_X=False),
+                                'lasso_cv': sklearn.linear_model.LassoCV(copy_X=False)
+                            }[pair_scoring]
+                            x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y,
+                                                                                                        shuffle=False)
+                            reg.fit(x_train, y_train)
+                            pair_scores = flattener.inverse_transform(reg.coef_)[0]
+                            symbol_type = {
+                                'predicate_precedence': 'predicate',
+                                'function_precedence': 'function'
+                            }[precedence_option]
+                            perm = precedence.learn_ltot(pair_scores, symbol_type=symbol_type)
+                            json.dump({
+                                'name': name,
+                                'symbol_type': symbol_type,
+                                'n_nan_pairs': np.count_nonzero(np.isnan(pair_scores)),
+                                'instance': str(reg),
+                                'train': reg.score(x_train, y_train),
+                                'test': reg.score(x_test, y_test),
+                                'greedy_precedence': str(perm)
+                            }, sys.stderr, indent=4)
+                            stats[(symbol_type, 'train')] = reg.score(x_train, y_train)
+                            stats[(symbol_type, 'test')] = reg.score(x_test, y_test)
+                            stats[(symbol_type, 'nonzero_coefs')] = np.count_nonzero(reg.coef_)
+                            if pair_scoring in ['lasso', 'lasso_cv']:
+                                stats[(symbol_type, 'n_iter')] = reg.n_iter_
+                                if pair_scoring == 'lasso_cv':
+                                    stats[(symbol_type, 'alpha')] = reg.alpha_
+                                    stats[(symbol_type, 'alphas.min')] = reg.alphas_.min()
+                                    stats[(symbol_type, 'alphas.max')] = reg.alphas_.max()
+                            good_precedences[precedence_option] = perm
+                            try:
+                                output_dir = os.path.join(output_batch, 'ltot', name)
+                                precedence.plot_preference_heatmap(pair_scores, perm,
+                                                                   symbol_type, problem,
+                                                                   output_file=os.path.join(output_dir, 'preferences',
+                                                                                            f'{problem.name()}_{symbol_type}'))
+                            except ValueError:
+                                logging.debug('Preference heatmap plotting failed.', exc_info=True)
+                        execution = problem.get_execution(precedences=good_precedences)
                         logging.info({'name': name, 'status': execution['status'], 'exit_code': execution['exit_code'],
                                       'saturation_iterations': execution['saturation_iterations']})
                         if execution.result.exit_code == 0:
@@ -219,9 +295,13 @@ def call(namespace):
                             custom_dfs[name] = list()
                         df = execution.get_dataframe(field_names_obligatory=vampyre.vampire.Execution.field_names_solve)
                         df['log_scale'] = log_scale
-                        df['failure_base'] = failure_base
-                        df['failure_factor'] = failure_factor
-                        df['failure_score'] = failure_score
+                        df['normalize'] = normalize
+                        df['failure_penalty_quantile'] = failure_penalty_quantile
+                        df['failure_penalty_factor'] = failure_penalty_factor
+                        df['pair_scoring'] = pair_scoring
+                        df['alpha'] = alpha
+                        for key, value in stats.items():
+                            df[key] = value
                         custom_dfs[name].append(df)
                 try:
                     plot_saturation_iterations_distribution(saturation_iterations, problem, len(executions),
--- vampire_ml/precedence.py
+++ vampire_ml/precedence.py
@@ -1,107 +1,47 @@
 #!/usr/bin/env python3.7
 
-import logging
 import os
 
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-from utils import numpy_err_settings, truncate
+from utils import truncate
 
 
-def learn_precedence_lex(precedence_scores, symbol_type, problem=None, output_dir=None, failure_score='dominate'):
-    pair_hits, pair_failures, pair_score_sum = compute_pairwise_scores(precedence_scores)
-    n = pair_hits.shape[0]
-    assert (n, n) == pair_hits.shape == pair_failures.shape == pair_score_sum.shape
-    assert np.all(pair_hits >= pair_failures)
-    assert np.all(pair_failures >= 0)
-    pair_successes = pair_hits - pair_failures
-    if failure_score == 'dominate':
-        with numpy_err_settings(invalid='ignore'):
-            # Preference matrix. `v[i, j]` is the expected score of a run with the symbol pair (i, j).
-            pair_failure_rate = pair_failures / pair_hits
-    else:
-        if failure_score != 'ignore':
-            pair_score_sum += pair_failures * failure_score
-            pair_successes = pair_hits
-        pair_failure_rate = np.zeros(pair_hits.shape, dtype=np.float)
-    with numpy_err_settings(invalid='ignore'):
-        # Preference matrix. `v[i, j]` is the expected score of a run with the symbol pair (i, j).
-        pair_mean_score = pair_score_sum / pair_successes
+def learn_ltot(pair_scores, symbol_type=None):
     if symbol_type == 'predicate':
         head = np.asarray([0], dtype=np.uint)
-        tail = construct_good_permutation_lex(pair_failure_rate[1:, 1:], pair_mean_score[1:, 1:]) + 1
+        tail = ltot_construct_permutation(pair_scores[1:, 1:]) + 1
         assert np.all(tail > 0)
         perm = np.concatenate((head, tail))
     else:
-        perm = construct_good_permutation_lex(pair_failure_rate, pair_mean_score)
-    assert perm.shape == (n,)
-    try:
-        plot_preference_heatmap(pair_failure_rate, perm, symbol_type, problem,
-                                output_file=os.path.join(output_dir, 'preferences',
-                                                         f'{problem.name()}_{symbol_type}_failures'))
-        plot_preference_heatmap(pair_mean_score, perm, symbol_type, problem,
-                                output_file=os.path.join(output_dir, 'preferences',
-                                                         f'{problem.name()}_{symbol_type}_score'))
-    except ValueError:
-        logging.debug('Preference heatmap plotting failed.', exc_info=True)
+        perm = ltot_construct_permutation(pair_scores)
     return perm
 
 
-def compute_pairwise_scores(precedence_scores):
-    precedence_scores = list(precedence_scores)
-    n = len(precedence_scores[0][0])
-    # `pair_hits[i, j]` is the number of hits of the symbol pair (i, j).
-    pair_hits = np.zeros((n, n), dtype=np.uint)
-    # `pair_failures[i, j]` is the number of failures with the symbol pair (i, j).
-    pair_failures = np.zeros((n, n), dtype=np.uint)
-    # `pair_score_sum[i, j]` is the cumulative score of the symbol pair (i, j).
-    pair_score_sum = np.zeros((n, n), dtype=np.float)
-    for precedence, score in precedence_scores:
-        precedence_inverse = invert_permutation(precedence)
-        pairs_hit_now = np.tri(n, k=-1, dtype=np.uint).transpose()[precedence_inverse, :][:, precedence_inverse]
-        pair_hits += pairs_hit_now
-        if not np.isnan(score):
-            pair_score_sum += pairs_hit_now * score
-        else:
-            pair_failures += pairs_hit_now
-    return pair_hits, pair_failures, pair_score_sum
-
-
-def invert_permutation(p):
-    # https://stackoverflow.com/a/25535723/4054250
-    s = np.empty(p.size, p.dtype)
-    s[p] = np.arange(p.size)
-    return s
-
-
-def construct_good_permutation_lex(failure_rate, average_iterations):
+def ltot_construct_permutation(pair_scores, nan=None):
     """Find good permutation greedily."""
-    n = failure_rate.shape[0]
-    assert (n, n) == failure_rate.shape == average_iterations.shape
+    n = pair_scores.shape[0]
+    assert (n, n) == pair_scores.shape
     perm = np.empty(n, dtype=np.uint)
     if n == 0:
         return perm
+    if nan is None:
+        assert np.any(~np.isnan(pair_scores))
+        nan = np.nanmean(pair_scores)
+    pair_scores = np.nan_to_num(pair_scores, nan=nan)
     # s[i] = total score for row i - total score for column i
     # s[i] is the score we get by picking symbol i as the first symbol.
     # Symbol i should be picked as the first greedily if it minimizes s[i].
-    failure_rate = np.nan_to_num(failure_rate, nan=np.nanmean(failure_rate))
-    # TODO: Get rid of the warning with all-nan average_iterations.
-    average_iterations = np.nan_to_num(average_iterations, nan=np.nanmean(average_iterations))
-    s_failure_rate = np.sum(failure_rate, axis=1).flatten() - np.sum(failure_rate, axis=0).flatten()
-    s_average_iterations = np.sum(average_iterations, axis=1).flatten() - np.sum(average_iterations, axis=0).flatten()
+    point_scores = np.sum(pair_scores, axis=1).flatten() - np.sum(pair_scores, axis=0).flatten()
     # https://papers.nips.cc/paper/1431-learning-to-order-things.pdf
     for i in range(n):
-        minimum_failure_rate_indices = np.flatnonzero(np.isclose(s_failure_rate, np.min(s_failure_rate)))
-        cur = minimum_failure_rate_indices[np.argmin(s_average_iterations[minimum_failure_rate_indices])]
-        assert s_failure_rate[cur] != np.inf
-        assert s_average_iterations[cur] != np.inf
+        cur = np.argmin(point_scores)
+        assert point_scores[cur] != np.inf
         perm[i] = cur
-        s_failure_rate += failure_rate[cur, :] - failure_rate[:, cur]
-        s_failure_rate[cur] = np.inf
-        s_average_iterations += average_iterations[cur, :] - average_iterations[:, cur]
-        s_average_iterations[cur] = np.inf
+        point_scores += pair_scores[cur, :] - pair_scores[:, cur]
+        point_scores[cur] = np.inf
     return perm
 
 
