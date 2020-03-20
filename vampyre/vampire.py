56c8a45a90ec20ab34d158386e7b765423428a2d
Filip Bartek <filip.bartek@hotmail.com>
2020-03-20 16:38:01 +0100

    Autoformat some code

--- vampyre/vampire.py
+++ vampyre/vampire.py
@@ -230,7 +230,8 @@ class Problem:
                         reversed_precedences['predicate_precedence'] = np.concatenate((head, tail))
                     if random_functions:
                         reversed_precedences['function_precedence'] = precedences['function_precedence'][::-1]
-                    t.set_postfix({'current_configuration': self.get_configuration_path(precedences=reversed_precedences)})
+                    t.set_postfix(
+                        {'current_configuration': self.get_configuration_path(precedences=reversed_precedences)})
                     execution = self.get_execution(precedences=reversed_precedences)
                     assert execution.path == self.get_configuration_path(precedences=reversed_precedences)
                     yield execution
