cdafa5598e4325a61489fd659467e6350698a369
Filip Bartek <filip.bartek@hotmail.com>
2020-03-31 10:45:05 +0200

    Fix: LinearSVR: Use squared loss
    for compatibility with `dual=False`.

--- vampire_ml/fit.py
+++ vampire_ml/fit.py
@@ -157,7 +157,7 @@ def call(namespace):
         else:
             reg_linear = LinearRegression(copy_X=False)
             reg_mlp = MLPRegressor(random_state=0, early_stopping=True, verbose=True)
-            reg_svr_linear = LinearSVR(dual=False, random_state=0, verbose=1000)
+            reg_svr_linear = LinearSVR(loss='squared_epsilon_insensitive', dual=False, random_state=0, verbose=1000)
             reg_svr = SVR(verbose=True)
             param_grid = [
                 {},
