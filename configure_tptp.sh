e3b507c2a8ecc3e31ca304df8ab9ecc7c4838dfa
Filip Bartek <filip.bartek@hotmail.com>
2020-04-14 14:29:18 +0200

    Collect a list of problems when setting up TPTP

--- configure_tptp.sh
+++ configure_tptp.sh
@@ -10,4 +10,4 @@ TPTP_TGZ="$TPTP_VERSION.tgz"
 wget "http://www.tptp.org/TPTP/Distribution/$TPTP_TGZ" -P /tmp
 tar -xzf "/tmp/$TPTP_TGZ" -C "$HOME"
 
-#find "$TPTP_PROBLEMS" -regextype sed -regex ".*/[A-Z]\{3\}[0-9]\{3\}[-+][1-9][0-9]*\(\.[0-9]\{3\}\)*\.p" -exec realpath --relative-to "$TPTP_PROBLEMS" {} + > problems_all.txt
+find "$TPTP_PROBLEMS" -regextype sed -regex ".*/[A-Z]\{3\}[0-9]\{3\}[-+][1-9][0-9]*\(\.[0-9]\{3\}\)*\.p" -exec realpath --relative-to "$TPTP_PROBLEMS" {} + | sort > problems_cnf_fof.txt
