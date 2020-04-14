94fcaff7135beb977c54e6c62a92460faf519098
Filip Bartek <filip.bartek@hotmail.com>
2020-04-14 18:36:10 +0200

    fit-array.sh: Clean up the script
    Improve debug output.

--- fit-array.sh
+++ fit-array.sh
@@ -14,6 +14,7 @@ export VAMPIRE_TIME_LIMIT=${VAMPIRE_TIME_LIMIT:-10}
 export SOLVE_RUNS_PER_PROBLEM=${SOLVE_RUNS_PER_PROBLEM:-1000}
 
 COMMON_SBATCH_OPTIONS=(
+  --parsable
   "--comment=$(git rev-parse --verify HEAD)"
 )
 
@@ -21,43 +22,50 @@ OUTPUT_SLURM=${OUTPUT_SLURM:-$OUTPUT/slurm}
 mkdir -p "$OUTPUT_SLURM"
 
 PROBLEMS=${PROBLEMS:-problems_selected_aggregated.txt}
-REDUCE_CPUS_PER_TASK=${REDUCE_CPUS_PER_TASK:-1}
+PROBLEMS_COUNT=$(wc -l <"$PROBLEMS")
+echo "PROBLEMS_COUNT=$PROBLEMS_COUNT"
 
 if [ -n "${SKIP_MAP-}" ]; then
   echo "Skipping map step (array job)."
-  sbatch "${COMMON_SBATCH_OPTIONS[@]}" --job-name="fit:reduce" --output="$OUTPUT_SLURM/%j.out" fit.sh --problem-list "$PROBLEMS" "$@"
-  JOB_ID=$(sbatch "${COMMON_SBATCH_OPTIONS[@]}" --cpus-per-task="$REDUCE_CPUS_PER_TASK" --job-name="fit:reduce" --output="$OUTPUT_SLURM/%j.out" --parsable fit.sh --problem-list "$PROBLEMS" "$@")
-
-  echo Job ID: "$JOB_ID"
-
-  BATCHES_DIR="$OUTPUT/batches/$JOB_ID"
-  mkdir -p "$BATCHES_DIR"
-  git rev-parse --verify HEAD >"$BATCHES_DIR/git-commit-sha.txt"
-  env | sort >"$BATCHES_DIR/env.txt"
-  echo "$@" >"$BATCHES_DIR/parameters.txt"
 else
-  PROBLEMS_COUNT=$(wc -l <"$PROBLEMS")
-  MAP_CPUS_PER_TASK=${MAP_CPUS_PER_TASK:-1}
   MAX_ARRAY_SIZE=$(scontrol show config | grep MaxArraySize | awk '{split($0, a, "="); print a[2]}' | sed 's/^ *//g')
   # https://stackoverflow.com/a/10415158/4054250
   ARRAY_TASK_COUNT=${ARRAY_TASK_COUNT:-$((PROBLEMS_COUNT > MAX_ARRAY_SIZE ? MAX_ARRAY_SIZE : PROBLEMS_COUNT))}
   export PROBLEM_MODULUS=${PROBLEM_MODULUS:-$ARRAY_TASK_COUNT}
   ARRAY=${ARRAY:-0-$((ARRAY_TASK_COUNT - 1))}
-  TIME_PER_TASK=${TIME_PER_TASK:-$((PROBLEMS_COUNT * (SOLVE_RUNS_PER_PROBLEM + 1) * (VAMPIRE_TIME_LIMIT + 10) / (ARRAY_TASK_COUNT * 60)))}
 
-  echo "PROBLEMS_COUNT=$PROBLEMS_COUNT"
+  MAP_CPUS_PER_TASK=${MAP_CPUS_PER_TASK:-1}
+  echo "MAP_CPUS_PER_TASK=$MAP_CPUS_PER_TASK"
+
+  TIME_PER_TASK=${TIME_PER_TASK:-$((PROBLEMS_COUNT * (SOLVE_RUNS_PER_PROBLEM + 1) * (VAMPIRE_TIME_LIMIT + 10) / (ARRAY_TASK_COUNT * 60)))}
   echo "TIME_PER_TASK=$TIME_PER_TASK"
 
-  ARRAY_JOB_ID=$(sbatch "${COMMON_SBATCH_OPTIONS[@]}" --cpus-per-task="$MAP_CPUS_PER_TASK" --job-name="fit:map" --output="$OUTPUT_SLURM/%A_%a.out" --parsable --input="$PROBLEMS" --time="$TIME_PER_TASK" --mem-per-cpu=$((VAMPIRE_MEMORY_LIMIT + 128)) --array="$ARRAY" fit.sh "$@" --precompute)
+  MAP_JOB_ID=$(sbatch "${COMMON_SBATCH_OPTIONS[@]}" --job-name="fit:map" --output="$OUTPUT_SLURM/%A_%a.out" --cpus-per-task="$MAP_CPUS_PER_TASK" --input="$PROBLEMS" --time="$TIME_PER_TASK" --mem-per-cpu=$((VAMPIRE_MEMORY_LIMIT + 128)) --array="$ARRAY" fit.sh "$@" --precompute)
+  echo "MAP_JOB_ID=$MAP_JOB_ID"
 
-  echo Array job ID: "$ARRAY_JOB_ID"
+  MAP_BATCHES_DIR="$OUTPUT/batches/$MAP_JOB_ID"
+  mkdir -p "$MAP_BATCHES_DIR"
+  git rev-parse --verify HEAD >"$MAP_BATCHES_DIR/git-commit-sha.txt"
+  env | sort >"$MAP_BATCHES_DIR/env.txt"
+  echo "$@" >"$MAP_BATCHES_DIR/parameters.txt"
+fi
 
-  BATCHES_DIR="$OUTPUT/batches/$ARRAY_JOB_ID"
-  mkdir -p "$BATCHES_DIR"
-  git rev-parse --verify HEAD >"$BATCHES_DIR/git-commit-sha.txt"
-  env | sort >"$BATCHES_DIR/env.txt"
-  echo "$@" >"$BATCHES_DIR/parameters.txt"
+if [ -n "${SKIP_REDUCE-}" ]; then
+  echo "Skipping reduce step."
+else
+  REDUCE_CPUS_PER_TASK=${REDUCE_CPUS_PER_TASK:-1}
+  echo "REDUCE_CPUS_PER_TASK=$REDUCE_CPUS_PER_TASK"
 
-  export ARRAY_JOB_ID
-  sbatch "${COMMON_SBATCH_OPTIONS[@]}" --cpus-per-task="$REDUCE_CPUS_PER_TASK" --job-name="fit:reduce" --output="$OUTPUT_SLURM/%j.out" --dependency=afterok:"$ARRAY_JOB_ID" fit.sh --problem-list "$PROBLEMS" "$@"
+  if [ -n "${MAP_JOB_ID}" ]; then
+    DEPENDENCY_OPTION="--dependency=afterok:$MAP_JOB_ID"
+  fi
+
+  REDUCE_JOB_ID=$(sbatch "${COMMON_SBATCH_OPTIONS[@]}" --job-name="fit:reduce" --output="$OUTPUT_SLURM/%j.out" --cpus-per-task="$REDUCE_CPUS_PER_TASK" ${DEPENDENCY_OPTION-} fit.sh --problem-list "$PROBLEMS" "$@")
+  echo "REDUCE_JOB_ID=$REDUCE_JOB_ID"
+
+  REDUCE_BATCHES_DIR="$OUTPUT/batches/$REDUCE_JOB_ID"
+  mkdir -p "$REDUCE_BATCHES_DIR"
+  git rev-parse --verify HEAD >"$REDUCE_BATCHES_DIR/git-commit-sha.txt"
+  env | sort >"$REDUCE_BATCHES_DIR/env.txt"
+  echo "$@" >"$REDUCE_BATCHES_DIR/parameters.txt"
 fi
