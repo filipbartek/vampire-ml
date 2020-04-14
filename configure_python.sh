3a675ac7c4d2c2592866d0d474db6ce503380f62
Filip Bartek <filip.bartek@hotmail.com>
2020-04-14 14:28:21 +0200

    Depend on python3 instead of python3.7

--- configure_python.sh
+++ configure_python.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 source env.sh
 
 if [ -n "${MODULESHOME-}" ]; then module load Python; fi
-if [ ! -e "$VAMPIRE_ML_VENV" ]; then python3.7 -m virtualenv "$VAMPIRE_ML_VENV"; fi
+if [ ! -e "$VAMPIRE_ML_VENV" ]; then python3 -m virtualenv "$VAMPIRE_ML_VENV"; fi
 source "$VAMPIRE_ML_VENV/bin/activate"
 pip install -r requirements.txt
