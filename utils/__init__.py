1cbc04a44389af72fb2f7975c05011a61f5d0cc3
Filip Bartek <filip.bartek@hotmail.com>
2020-04-20 19:05:50 +0200

    Reorder utils/__init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
+from .memory import memory
+from .progress_bar import ProgressBar
 from .utils import makedirs_open, get_consistent, len_robust, fill_category_na, numpy_err_settings, truncate, \
     dict_to_name
-from .progress_bar import ProgressBar
-from .memory import memory
