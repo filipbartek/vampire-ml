e411045878c4b9d12ab064b5ce79f4a27b0c7a2a
Filip Bartek <filip.bartek@hotmail.com>
2020-05-07 22:26:37 +0200

    Fix a path to a problem list

--- map-reduce/run-predicate.sh
+++ map-reduce/run-predicate.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 export OUTPUT=${OUTPUT:-$(dirname $0)/predicate}
 export VAMPIRE_ML=${VAMPIRE_ML:-$(dirname $0)/..}
-export PROBLEMS=${PROBLEMS:-$VAMPIRE_ML/problems/problems/predicate-small-10000.txt}
+export PROBLEMS=${PROBLEMS:-$VAMPIRE_ML/problems/predicate-small-10000.txt}
 export PROBLEMS_TRAIN=${PROBLEMS_TRAIN:-$VAMPIRE_ML/problems/predicate-easy-variation.txt}
 
 $(dirname $0)/run.sh --random-predicate-precedence "$@"
