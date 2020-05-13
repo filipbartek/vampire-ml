f966b1bc114170a4029b5fc8aa7ef2032e7ba7a0
Filip Bartek <filip.bartek@hotmail.com>
2020-05-13 15:53:46 +0200

    Document the xargs call in reduce.sh

--- map-reduce/reduce.sh
+++ map-reduce/reduce.sh
@@ -16,6 +16,7 @@ echo Parameters: "$@"
 
 if [ -n "${REDUCE_CASES-}" ]; then
   CASE_COUNT=$(wc -l <$REDUCE_CASES)
+  # We assume that there are exactly 2 options on each line.
   cat $REDUCE_CASES | xargs --max-args=2 --max-procs=$CASE_COUNT --process-slot-var=JOB_ID --verbose $(realpath $VAMPIRE_ML/fit.sh) "$@"
 else
   $(realpath $VAMPIRE_ML/fit.sh) "$@"
