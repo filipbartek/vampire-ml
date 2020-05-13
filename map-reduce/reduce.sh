7a4496ee078967a7ffdfd701fb349fcd3c6cef42
Filip Bartek <filip.bartek@hotmail.com>
2020-05-13 15:53:11 +0200

    reduce.sh: Fix case param distribution
    We assume that there are exactly 2 options
    on each line.

--- map-reduce/reduce.sh
+++ map-reduce/reduce.sh
@@ -16,7 +16,7 @@ echo Parameters: "$@"
 
 if [ -n "${REDUCE_CASES-}" ]; then
   CASE_COUNT=$(wc -l <$REDUCE_CASES)
-  cat $REDUCE_CASES | tr '\n' '\0' | xargs -0 --max-args=1 --max-procs=$CASE_COUNT --process-slot-var=JOB_ID --verbose $(realpath $VAMPIRE_ML/fit.sh) "$@"
+  cat $REDUCE_CASES | xargs --max-args=2 --max-procs=$CASE_COUNT --process-slot-var=JOB_ID --verbose $(realpath $VAMPIRE_ML/fit.sh) "$@"
 else
   $(realpath $VAMPIRE_ML/fit.sh) "$@"
 fi
