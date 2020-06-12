1909f0c97768c59141567064c7d8c4f4d33b631a
Filip Bartek <filip.bartek@hotmail.com>
2020-06-12 12:34:12 +0200

    Use `chain.from_iterable` instead of `chain`

--- proving/file_path_list.py
+++ proving/file_path_list.py
@@ -20,7 +20,7 @@ def normalize_path(path, base_path=None):
 def paths_from_sublists(sublist_file_paths, base_path=None):
     files = map(open, sublist_file_paths)
     line_generators = (file.readlines() for file in files)
-    lines = itertools.chain(*line_generators)
+    lines = itertools.chain.from_iterable(line_generators)
     lines_stripped = (l.strip('\n') for l in lines)
     paths = map(partial(normalize_path, base_path=base_path), lines_stripped)
     return paths
