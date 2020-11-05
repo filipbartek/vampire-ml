b2f8506a85e2e59836cac5bac5b041e931e56481
Filip Bartek <filip.bartek@hotmail.com>
2020-11-05 13:50:35 +0100

    GraphSymbolFeatures: Make dynamic
    because graphification is run in call().

--- questions/models/symbol_features/graph.py
+++ questions/models/symbol_features/graph.py
@@ -6,12 +6,13 @@ from .symbol_features import SymbolFeatures
 
 class GraphSymbolFeatures(SymbolFeatures, HeteroGCN):
     def __init__(self, graphifier, symbol_type, edge_layer_sizes=64, node_layer_sizes=64, num_layers=1):
-        SymbolFeatures.__init__(self)
+        SymbolFeatures.__init__(self, dynamic=True)
         if isinstance(edge_layer_sizes, int):
             edge_layer_sizes = {canonical_etype: edge_layer_sizes for canonical_etype in graphifier.canonical_etypes}
         if isinstance(node_layer_sizes, int):
             node_layer_sizes = {ntype: node_layer_sizes for ntype in graphifier.ntypes}
-        HeteroGCN.__init__(self, edge_layer_sizes, node_layer_sizes, num_layers, output_ntypes=[symbol_type])
+        HeteroGCN.__init__(self, edge_layer_sizes, node_layer_sizes, num_layers, output_ntypes=[symbol_type],
+                           dynamic=True)
         self.graphifier = graphifier
         self.symbol_type = symbol_type
 
