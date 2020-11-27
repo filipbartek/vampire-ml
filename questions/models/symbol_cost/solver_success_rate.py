49b181d21a9f74b1f5ca3ec5874320027b5080f1
Filip Bartek <filip.bartek@hotmail.com>
2020-11-27 15:23:08 +0100

    Parallelize evaluation with Vampire
    Use joblib to parallelize.
    TensorFlow does not seem to parallelize calls to Python functions.

--- questions/models/symbol_cost/solver_success_rate.py
+++ questions/models/symbol_cost/solver_success_rate.py
@@ -1,8 +1,12 @@
+import itertools
+
 import pandas as pd
 import tensorflow as tf
+from joblib import Parallel, delayed
 
 from proving import vampire
 from proving.utils import dataframe_from_records
+from proving.utils import py_str
 
 
 class ValidityRate(tf.keras.metrics.Mean):
@@ -34,34 +38,40 @@ class SolverSuccessRate(tf.keras.metrics.Mean):
     def update_state(self, problems, symbol_costs=None, valid=None, **kwargs):
         if len(problems.shape) == 2:
             problems = tf.squeeze(problems, axis=1)
-        if valid is not None:
+        if symbol_costs is not None and valid is not None:
+            # Ignore the invalid symbol costs. Only measure performance on the valid symbol costs.
             problems = tf.boolean_mask(problems, valid)
             symbol_costs = tf.ragged.boolean_mask(symbol_costs, valid)
-            values = tf.map_fn(self.evaluate_one, (problems, symbol_costs), fn_output_signature=tf.bool)
+            values = tf.py_function(self.solve_multiple, [problems, symbol_costs.flat_values, symbol_costs.row_splits],
+                                    tf.bool)
         else:
-            values = tf.map_fn(self.evaluate_one_baseline, problems, fn_output_signature=tf.bool)
+            # Evaluate the baseline solver performance
+            values = tf.py_function(self.solve_multiple, [problems], tf.bool)
         super().update_state(values, **kwargs)
 
-    def evaluate_one(self, x):
-        problem, symbol_cost = x
-        precedence = tf.argsort(symbol_cost)
-        return tf.py_function(self.solve_one, [problem, precedence], tf.bool)
+    def solve_multiple(self, problems, flat_values=None, row_splits=None):
+        if flat_values is None or row_splits is None:
+            symbol_costs = itertools.repeat(None)
+        else:
+            symbol_costs = tf.RaggedTensor.from_row_splits(flat_values, row_splits)
+        records = Parallel(verbose=0)(
+            delayed(self.solve_one)(problem, symbol_cost) for problem, symbol_cost in zip(problems, symbol_costs))
+        self.records.extend(records)
+        return [r['returncode'] == 0 for r in records]
 
-    def evaluate_one_baseline(self, problem):
-        return tf.py_function(self.solve_one, [problem], tf.bool)
-
-    def solve_one(self, problem, precedence=None):
-        problem = bytes.decode(problem.numpy())
-        record = {'problem': problem, 'valid': True}
+    def solve_one(self, problem, symbol_cost=None):
+        problem = py_str(problem)
+        record = {'problem': problem, 'valid': symbol_cost is not None}
         precedences = None
-        if precedence is not None:
+        if symbol_cost is not None:
+            # We sort the symbols by cost in non-decreasing order.
+            precedence = tf.argsort(symbol_cost)
             precedence = precedence.numpy()
             precedences = {self.symbol_type: precedence}
             record['n_symbols'] = len(precedence)
         solver_res = self.solver.solve(problem, precedences=precedences)
         record.update(solver_res.as_record())
-        self.records.append(record)
-        return solver_res.returncode == 0
+        return record
 
     def result_df(self):
         return dataframe_from_records(self.records, index_keys='problem', dtypes=self.record_pd_dtypes)
