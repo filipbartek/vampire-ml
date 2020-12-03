abd21d852371282060390f4c877b341286371886
Filip Bartek <filip.bartek@hotmail.com>
2020-12-03 12:16:52 +0100

    Improve description of the symbol cost regularizer

--- questions/models/symbol_cost/composite.py
+++ questions/models/symbol_cost/composite.py
@@ -23,7 +23,10 @@ class Composite(SymbolCostModel):
         costs_flat_values = self.embedding_to_cost(embeddings['embeddings'].flat_values, training=training)
         costs_flat_values = tf.squeeze(costs_flat_values, axis=1)
         costs = tf.RaggedTensor.from_nested_row_splits(costs_flat_values, embeddings['embeddings'].nested_row_splits)
-        # For each problem, its regularization loss is the l2-norm of the symbol cost vector.
+        # For each problem, its regularization loss is proportional to the l2-norm of the symbol cost vector.
+        # More precisely, it is `reduce_mean(square(x))` where x is the symbol cost vector.
+        # We take the mean over symbols instead of sum so that problems with large signature don't contribute more than problems with small signature.
+        # We aggregate across problems by taking the mean.
         valid_costs = tf.ragged.boolean_mask(costs, embeddings['valid'])
         self.add_loss(self.l2 * tf.reduce_mean(tf.reduce_mean(tf.square(valid_costs), axis=1)))
         return {'costs': costs, 'valid': embeddings['valid']}
