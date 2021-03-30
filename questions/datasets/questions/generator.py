09b33eeaaade64b030f1a7153751ba5cf5fe1968
Filip Bartek <filip.bartek@hotmail.com>
2021-03-31 01:33:32 +0200

    Capture more stats about problems when generating questions

--- questions/datasets/questions/generator.py
+++ questions/datasets/questions/generator.py
@@ -132,7 +132,8 @@ class Generator:
         except FileNotFoundError:
             results = collections.defaultdict(list)
             problem_stats = pd.DataFrame(0, index=self.problems,
-                                         columns=['misses', ('hits', 'returncode'), ('hits', 'score')])
+                                         columns=['misses', ('hits', 'returncode'), ('hits', 'score'),
+                                                  ('runs', 'success'), ('runs', 'fail')])
             num_loaded = 0
             for step in tqdm(range(self.step), desc='Loading question batches', unit='batch'):
                 if num_questions is not None and num_loaded >= num_questions:
@@ -145,6 +146,11 @@ class Generator:
                     if num_questions_per_problem is not None and len(
                             results[problem_name]) >= num_questions_per_problem:
                         continue
+                    for result in attempt[1]:
+                        if result.returncode == 0:
+                            problem_stats.loc[problem_name]['runs', 'success'] += 1
+                        else:
+                            problem_stats.loc[problem_name]['runs', 'fail'] += 1
                     question = self.get_question(attempt)
                     if question is None:
                         problem_stats.loc[problem_name, 'misses'] += 1
