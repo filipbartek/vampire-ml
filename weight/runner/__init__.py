6b27c7f81a08c502498baf993a39c48c7b284acd
Filip Bartek <filip.bartek@hotmail.com>
2022-08-31 18:15:01 +0200

    Log in `runner.run`

--- weight/runner/__init__.py
+++ weight/runner/__init__.py
@@ -1,8 +1,13 @@
+import logging
+
 from . import benchexec
 from . import subprocess
 
+log = logging.getLogger(__name__)
+
 
 def run(args, backend='subprocess', **kwargs):
+    log.debug(f'Running with {backend}: %s' % ' '.join(args))
     if backend == 'subprocess':
         result = subprocess.run(args, **kwargs)
     elif backend == 'benchexec':
