38f10c6860a823455fc5f720e9a0a6e6aa4dd314
Filip Bartek <filip.bartek@hotmail.com>
2022-09-04 18:35:21 +0200

    Improve evaluation
    - Evaluate baseline
    - Evaluate multiple Vampire configurations (differing in age_weight_ratio)
    - Keep problems without proofs in the validation dataset
    - Allow disabling the initial pre-training evaluation
    - Robustify against empty validation set

--- weight/train.py
+++ weight/train.py
@@ -5,6 +5,7 @@ import logging
 import os
 import sys
 import tempfile
+import warnings
 from collections import defaultdict
 
 import joblib
@@ -115,6 +116,13 @@ def load_proof(path, signature, max_size=None):
     return meta['problem'], samples
 
 
+def path_to_problem(path):
+    # Path format: runs/{problem}/{seed}/verbose
+    path_seed = os.path.dirname(path)
+    path_problem = os.path.dirname(path_seed)
+    return os.path.basename(path_problem)
+
+
 @hydra.main(config_path='.', config_name='config', version_base='1.1')
 def main(cfg):
     tf.random.set_seed(0)
@@ -126,29 +134,39 @@ def main(cfg):
 
     rng = np.random.default_rng(cfg.seed)
 
+    problem_names = pd.read_csv(hydra.utils.to_absolute_path(cfg.problems), names=['problem']).problem
+    problem_names = rng.permutation(problem_names)
+    if cfg.max_problem_count is not None:
+        problem_names = problem_names[:cfg.max_problem_count]
+
+    log.info(f'Number of problems: {len(problem_names)}')
+
+    problem_path_to_name = {questions.config.full_problem_path(name): name for name in problem_names}
+
+    def generate_verbose_paths(problem='*'):
+        return glob.glob(os.path.join(cfg.workspace_dir, 'runs', problem, '*', 'verbose'))
+
+    proof_paths = generate_verbose_paths()
+    problems_with_proofs = set(path_to_problem(path) for path in proof_paths)
+
+    train_count = int(len(problem_names) * cfg.validation_split)
+    problem_name_datasets = {
+        'train': problem_names[:train_count],
+        'val': problem_names[train_count:]
+    }
+    log.info('Number of problems: %s' % {k: len(v) for k, v in problem_name_datasets.items()})
+    problem_with_proof_name_datasets = {k: [p for p in v if p in problems_with_proofs] for k, v in
+                                        problem_name_datasets.items()}
+    log.info('Number of problems with proofs: %s' % {k: len(v) for k, v in problem_with_proof_name_datasets.items()})
+    problem_name_datasets['train'] = problem_with_proof_name_datasets['train']
+    log.info('Number of problems: %s' % {k: len(v) for k, v in problem_name_datasets.items()})
+    problem_names = list(itertools.chain.from_iterable(problem_name_datasets.values()))
+    log.info(f'Number of problems: {len(problem_names)}')
+
     with joblib.parallel_backend(cfg.parallel.backend, n_jobs=cfg.parallel.n_jobs):
         parallel = joblib.Parallel(verbose=cfg.parallel.verbose)
 
         clausifier = Solver()
-
-        def generate_verbose_paths(problem='*'):
-            return glob.glob(os.path.join(cfg.workspace_dir, 'runs', problem, '*', 'verbose'))
-
-        verbose_paths_all = generate_verbose_paths()
-
-        def path_to_problem(path):
-            # Path format: runs/{problem}/{seed}/verbose
-            path_seed = os.path.dirname(path)
-            path_problem = os.path.dirname(path_seed)
-            return os.path.basename(path_problem)
-
-        problem_names = sorted(set(path_to_problem(path) for path in verbose_paths_all))
-        problem_names = rng.permutation(problem_names)
-        if cfg.max_problem_count is not None:
-            problem_names = problem_names[:cfg.max_problem_count]
-        problem_path_to_name = {questions.config.full_problem_path(name): name for name in problem_names}
-
-        log.info(f'Number of problems: {len(problem_names)}')
 
         def get_signature(problem):
             try:
@@ -161,8 +179,15 @@ def main(cfg):
         signatures = parallel(joblib.delayed(get_signature)(problem) for problem in problem_names)
         problem_to_signature = dict(zip(problem_names, signatures))
 
+        all_problem_names = itertools.chain.from_iterable(problem_name_datasets.values())
+        problem_signatures = {path: problem_to_signature[path] for path in all_problem_names}
+
+        if cfg.evaluate.baseline:
+            evaluate(None, problem_signatures, cfg, parallel, problem_name_datasets, 'baseline')
+
         def generate_paths(problem_to_signature):
             for problem_name, signature in problem_to_signature.items():
+                # We automatically omit problems that do not have any proof.
                 for path in generate_verbose_paths(problem_name):
                     yield path, signature
 
@@ -170,18 +195,19 @@ def main(cfg):
             raise RuntimeError('Input workspace directory path is required.')
         print(f'Loading proofs of {len(problem_to_signature)} problems', file=sys.stderr)
         proof_traces = parallel(
-            joblib.delayed(load_proof)(path, signature, cfg.max_proof_stdout_size) for path, signature in generate_paths(problem_to_signature))
+            joblib.delayed(load_proof)(path, signature, cfg.max_proof_stdout_size) for path, signature in
+            generate_paths(problem_to_signature))
 
         problem_samples = defaultdict(list)
         for path, samples in proof_traces:
             if samples is None:
                 continue
-            problem_samples[path].append(samples)
+            problem_samples[problem_path_to_name[path]].append(samples)
 
         log.info(f'Number of problems with some samples: {len(problem_samples)}')
 
         graphifier = Graphifier(clausifier, max_number_of_nodes=10000)
-        graphs, graphs_df = graphifier.get_graphs_dict(problem_samples)
+        graphs, graphs_df = graphifier.get_graphs_dict(problem_names)
         log.info(f'Number of graphs: {len(graphs)}')
 
         output_ntypes = ['predicate', 'function']
@@ -209,16 +235,11 @@ def main(cfg):
 
         model_logit.compile(optimizer=optimizer)
 
-        problem_names = rng.permutation(list(problem_samples.keys()))
-        train_count = int(len(problem_names) * cfg.validation_split)
-        problem_name_datasets = {
-            'train': problem_names[:train_count],
-            'val': problem_names[train_count:]
-        }
         datasets_batched = {
-            dataset_name: dict_to_batches({problem_name: problem_samples[problem_name] for problem_name in problem_names},
-                                          cfg.batch.size).cache() for dataset_name, problem_names in
-            problem_name_datasets.items()}
+            dataset_name: dict_to_batches(
+                {problem_name: problem_samples[problem_name] for problem_name in problem_names},
+                cfg.batch.size).cache() for dataset_name, problem_names in
+            problem_with_proof_name_datasets.items()}
 
         ckpt_dir = 'ckpt'
         log.info(f'Checkpoint directory: {ckpt_dir}')
@@ -226,30 +247,28 @@ def main(cfg):
             tf.keras.callbacks.ModelCheckpoint(
                 os.path.join(ckpt_dir, 'epoch', 'weights.{epoch:05d}.tf'),
                 save_weights_only=True, verbose=0),
-            tf.keras.callbacks.ModelCheckpoint(
-                os.path.join(ckpt_dir, 'acc', 'weights.{epoch:05d}-{val_binary_accuracy:.2f}.tf'),
-                save_weights_only=True, verbose=1, monitor='val_binary_accuracy', save_best_only=True),
             tf.keras.callbacks.EarlyStopping(**cfg.early_stopping),
             tf.keras.callbacks.ReduceLROnPlateau(**cfg.reduce_lr_on_plateau)
         ]
 
+        if len(problem_with_proof_name_datasets['val']) >= 1:
+            cbs.append(tf.keras.callbacks.ModelCheckpoint(
+                os.path.join(ckpt_dir, 'acc', 'weights.{epoch:05d}-{val_binary_accuracy:.2f}.tf'),
+                save_weights_only=True, verbose=1, monitor='val_binary_accuracy', save_best_only=True))
+
         def evaluate_all(era_dir):
+            log.info(f'Evaluating {era_dir}')
             for dataset_name, dataset in datasets_batched.items():
                 log.info(f'{dataset_name}: Evaluating...')
                 res = model_logit.evaluate(dataset, return_dict=True)
                 log.info(f'{dataset_name}: {res}')
-            all_problem_names = itertools.chain.from_iterable(problem_name_datasets.values())
-            problem_signatures = {path: problem_to_signature[problem_path_to_name[path]] for path in all_problem_names}
-            df = evaluate(model_symbol_weight, problem_signatures, cfg, parallel)
-            for dataset_name, problem_names in problem_name_datasets.items():
-                cur_df = df[df.problem.isin(problem_names)]
-                n_success = cur_df.szs_status.isin(['THM', 'CAX', 'UNS', 'SAT', 'CSA']).sum()
-                log.info(f'{dataset_name} empirical success count: {n_success}/{len(cur_df)}')
-            save_df(df, os.path.join(era_dir, 'problems'))
+            evaluate(model_symbol_weight, problem_signatures, cfg, parallel, problem_name_datasets,
+                     os.path.join(era_dir, 'problems'))
 
-        era_dir = os.path.join('era', str(0))
-        os.makedirs(era_dir, exist_ok=True)
-        evaluate_all(era_dir)
+        if cfg.evaluate.initial:
+            era_dir = os.path.join('era', str(0))
+            os.makedirs(era_dir, exist_ok=True)
+            evaluate_all(era_dir)
         for era in range(cfg.eras):
             era_dir = os.path.join('era', str(era))
             os.makedirs(era_dir, exist_ok=True)
@@ -259,35 +278,65 @@ def main(cfg):
             evaluate_all(era_dir)
 
 
-def evaluate(model, problem_signatures, cfg, parallel):
+def evaluate(model, problem_signatures, cfg, parallel, problem_name_datasets, out_dir):
+    model_result = None
+    if model is not None:
+        log.info('Evaluating a model')
+        problem_names = list(map(str, problem_signatures.keys()))
+        model_result = model.predict(problem_names)
+    else:
+        log.info('Evaluating baseline')
+
+    for eval_name, eval_options in cfg.options.evaluation.items():
+        log.info(f'Evaluating configuration {eval_name}')
+        df = evaluate_options(model_result, problem_signatures, cfg, eval_options, parallel)
+        for dataset_name, problem_names in problem_name_datasets.items():
+            df[f'dataset_{dataset_name}'] = df.problem.isin(problem_names)
+            cur_df = df[df.problem.isin(problem_names)]
+            n_success = cur_df.szs_status.isin(['THM', 'CAX', 'UNS', 'SAT', 'CSA']).sum()
+            log.info(f'{eval_name} {dataset_name} empirical success count: {n_success}/{len(cur_df)}')
+        save_df(df, os.path.join(out_dir, 'problems'))
+
+
+def evaluate_options(model_result, problem_signatures, cfg, eval_options, parallel):
     problem_names = list(map(str, problem_signatures.keys()))
-    res = model.predict(problem_names)
-    options = {**cfg.options.common, **cfg.options.probe}
+    options = {**cfg.options.common, **cfg.options.probe, **eval_options}
 
     def run(problem, valid, cost):
         log.debug(f'Attempting problem {problem}')
         result = {'problem': problem, 'valid': valid}
         if not valid:
             return result
-        weight = cost.numpy()
-        signature = problem_signatures[problem]
-        assert len(weight) == 4 + len(signature)
-        weights = {
-            'variable': weight[0],
-            'negation': weight[1],
-            'equality': weight[2],
-            'inequality': weight[3],
-            'symbol': dict(zip(signature, weight[4:]))
-        }
-        log.debug(f'{problem} {weights}')
+        if cost is not None:
+            weight = cost.numpy()
+            signature = problem_signatures[problem]
+            assert len(weight) == 4 + len(signature)
+            weights = {
+                'variable': weight[0],
+                'negation': weight[1],
+                'equality': weight[2],
+                'inequality': weight[3],
+                'symbol': dict(zip(signature, weight[4:]))
+            }
+            log.debug(f'{problem} {weights}')
+        else:
+            weights = None
         # result['weights'] = weights
-        run_result = vampire_run(problem, options, weights, vampire=cfg.vampire_cmd, **cfg.probe_run_args)
+        problem_path = questions.config.full_problem_path(problem)
+        try:
+            run_result = vampire_run(problem_path, options, weights, vampire=cfg.vampire_cmd, **cfg.probe_run_args)
+        except RuntimeError as e:
+            warnings.warn(str(e))
+            return None
         selected_properties = ['szs_status', 'terminationreason', 'returncode', 'elapsed', 'out_dir',
                                'stdout_len', 'stderr_len']
         result.update({k: run_result[k] for k in selected_properties if k in run_result})
         return result
 
-    cases = zip(problem_names, res['valid'], res['costs'])
+    if model_result is None:
+        cases = zip(problem_names, itertools.repeat(True), itertools.repeat(None))
+    else:
+        cases = zip(problem_names, model_result['valid'], model_result['costs'])
     print(f'Running {len(problem_names)} cases', file=sys.stderr)
     results = parallel(joblib.delayed(run)(problem, valid, cost) for problem, valid, cost in cases)
 
@@ -298,15 +347,19 @@ def vampire_run(problem_path, options, weights, *args, **kwargs):
     options = options.copy()
     if 'include' in options and options['include'] is None:
         del options['include']
-    options['variable_weight'] = weights['variable']
-    # TODO: Set weights for negation, equality, inequality.
-    with tempfile.NamedTemporaryFile('w+', suffix='.properties', prefix=os.path.join('vampire_functor_weights_')) as f:
-        for functor, weight in weights['symbol'].items():
-            if functor == '=':
-                continue
-            f.write(f'{functor}={weight}\n')
-        f.seek(0)
-        options['functor_weight'] = f.name
+    if weights is not None:
+        options['variable_weight'] = weights['variable']
+        # TODO: Set weights for negation, equality, inequality.
+        with tempfile.NamedTemporaryFile('w+', suffix='.properties',
+                                         prefix=os.path.join('vampire_functor_weights_')) as f:
+            for functor, weight in weights['symbol'].items():
+                if functor == '=':
+                    continue
+                f.write(f'{functor}={weight}\n')
+            f.seek(0)
+            options['functor_weight'] = f.name
+            return vampire.run(problem_path, options, *args, **kwargs)
+    else:
         return vampire.run(problem_path, options, *args, **kwargs)
 
 
@@ -325,9 +378,11 @@ def dict_to_batches(problems, batch_size):
         for b in more_itertools.chunked(gen_samples(), batch_size):
             x = {
                 'problem': to_tensor((row['problem'] for row in b), dtype=dtypes['problem'], name='problem'),
-                'occurrence_count': to_tensor((row['occurrence_count'] for row in b), dtype=dtypes['occurrence_count'], name='occurrence_count')
+                'occurrence_count': to_tensor((row['occurrence_count'] for row in b), dtype=dtypes['occurrence_count'],
+                                              name='occurrence_count')
             }
-            y = to_tensor((np.logical_not(np.squeeze(row['proof'].toarray(), axis=1)) for row in b), dtype=dtypes['nonproof'], name='nonproof')
+            y = to_tensor((np.logical_not(np.squeeze(row['proof'].toarray(), axis=1)) for row in b),
+                          dtype=dtypes['nonproof'], name='nonproof')
 
             flat_values = tf.constant(1, dtype=dtypes['sample_weight']) / tf.cast(
                 tf.repeat(y.row_lengths(), y.row_lengths()), dtypes['sample_weight'])
