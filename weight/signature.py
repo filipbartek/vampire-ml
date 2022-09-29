46cef12a16361d3a8e4fd586eb5082edf4f1571d
Filip Bartek <filip.bartek@hotmail.com>
2022-09-29 12:12:53 +0200

    Do not pass clausifier as argument to the parallelized signature fetcher
    to speed up the process
    because the arguments to the delayed function need to be pickled.

--- weight/signature.py
+++ weight/signature.py
@@ -8,13 +8,13 @@ from questions.memory import memory
 def get_signatures(problem_names, clausifier, parallel=None):
     if parallel is None:
         parallel = joblib.Parallel()
+
+    def get_signature(problem):
+        try:
+            # Raises `RuntimeError` when clausification fails
+            return clausifier.signature(problem)
+        except RuntimeError:
+            return None
+
     print(f'Collecting signatures of {len(problem_names)} problems', file=sys.stderr)
-    return parallel(joblib.delayed(get_signature)(problem, clausifier) for problem in problem_names)
-
-
-def get_signature(problem, clausifier):
-    try:
-        # Raises `RuntimeError` when clausification fails
-        return clausifier.signature(problem)
-    except RuntimeError:
-        return None
+    return parallel(joblib.delayed(get_signature)(problem) for problem in problem_names)
