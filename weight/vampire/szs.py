859242e77183d4b79eec78a5c9280631b858393b
Filip Bartek <filip.bartek@hotmail.com>
2022-11-16 11:52:28 +0100

    Factor SZS out into a dedicated file

---
+++ weight/vampire/szs.py
@@ -0,0 +1,68 @@
+# https://www.tptp.org/TPTP/TPTPTParty/2007/PositionStatements/GeoffSutcliffe_SZS.html
+
+import re
+
+short_to_long = {
+    # Unsatisfiable
+    'THM': 'Theorem',
+    'CAX': 'ContradictoryAxioms',
+    'UNS': 'Unsatisfiable',
+    # Satisfiable
+    'SAT': 'Satisfiable',
+    'CSA': 'CounterSatisfiable',
+    # Unsolved
+    'UNK': 'Unknown',
+    'ERR': 'Error',
+    'TMO': 'Timeout',
+    'MMO': 'MemoryOut',
+    'GUP': 'GaveUp',
+    'INC': 'Incomplete',
+    'IAP': 'Inappropriate'
+}
+
+long_to_short = {v: k for k, v in short_to_long.items()}
+
+termination_reason_to_status = {
+    'Refutation': 'UNS',
+    'Satisfiable': 'SAT',
+    'Time limit': 'TMO',
+    'Memory limit': 'MMO',
+    'Activation limit': 'ACO',
+    'Refutation not found, non-redundant clauses discarded': 'INC',
+    'Refutation not found, incomplete strategy': 'INC',
+    'Inappropriate': 'IAP',
+    'Unknown': 'UNK'
+}
+
+
+def from_output(output):
+    status_long = long_from_output_field(output)
+    try:
+        return long_to_short[status_long]
+    except KeyError:
+        term_reason = termination_reason(output)
+        if term_reason == 'Unknown':
+            if re.search(r'% Instruction limit reached!$', output, re.MULTILINE):
+                return 'INO'
+        try:
+            return termination_reason_to_status[term_reason]
+        except KeyError:
+            return 'UNK'
+
+
+def long_from_output_field(stdout):
+    # http://www.tptp.org/TPTP/TPTPTParty/2007/PositionStatements/GeoffSutcliffe_SZS.html
+    # Example:
+    # % SZS status Unsatisfiable for SET846-1
+    # `vampire --mode casc` always outputs exactly one SZS status.
+    m = re.search(r'^% SZS status (?P<status>.*) for (?P<problem>.*)$', stdout, re.MULTILINE)
+    if m is not None:
+        return m['status']
+    return None
+
+
+def termination_reason(stdout):
+    m = re.search(r'^% Termination reason: (.*)$', stdout, re.MULTILINE)
+    if m is not None:
+        return m[1]
+    return None
--- weight/vampire/vampire.py
+++ weight/vampire/vampire.py
@@ -5,6 +5,7 @@ import os
 import re
 from contextlib import suppress
 
+from . import szs
 from utils import to_str
 from weight import runner
 
@@ -22,7 +23,7 @@ def run(problem_path, options, out_dir=None, **kwargs):
             status_short = 'TMO'
     output = result['stdout']
     if status_short is None:
-        status_short = extract_status(output)
+        status_short = szs.from_output(output)
     result['szs_status'] = status_short
 
     with suppress(TypeError):
@@ -66,53 +67,6 @@ def run_bare(problem, options, vampire='vampire', **kwargs):
     return result
 
 
-def extract_status(output):
-    status_long = szs_status(output)
-    try:
-        return long_to_short[status_long]
-    except KeyError:
-        term_reason = termination_reason(output)
-        if term_reason == 'Unknown':
-            if re.search(r'% Instruction limit reached!$', output, re.MULTILINE):
-                return 'INO'
-        try:
-            return termination_reason_to_status[term_reason]
-        except KeyError:
-            return 'UNK'
-
-
-short_to_long = {
-    # Unsatisfiable
-    'THM': 'Theorem',
-    'CAX': 'ContradictoryAxioms',
-    'UNS': 'Unsatisfiable',
-    # Satisfiable
-    'SAT': 'Satisfiable',
-    'CSA': 'CounterSatisfiable',
-    # Unsolved
-    'UNK': 'Unknown',
-    'ERR': 'Error',
-    'TMO': 'Timeout',
-    'MMO': 'MemoryOut',
-    'GUP': 'GaveUp',
-    'INC': 'Incomplete',
-    'IAP': 'Inappropriate'
-}
-
-long_to_short = {v: k for k, v in short_to_long.items()}
-
-
-def szs_status(stdout):
-    # http://www.tptp.org/TPTP/TPTPTParty/2007/PositionStatements/GeoffSutcliffe_SZS.html
-    # Example:
-    # % SZS status Unsatisfiable for SET846-1
-    # `vampire --mode casc` always outputs exactly one SZS status.
-    m = re.search(r'^% SZS status (?P<status>.*) for (?P<problem>.*)$', stdout, re.MULTILINE)
-    if m is not None:
-        return m['status']
-    return None
-
-
 def save_result(out_dir, result):
     if out_dir is None:
         return
@@ -128,23 +82,3 @@ def save_result(out_dir, result):
         with open(os.path.join(out_dir, 'stderr.txt'), 'w') as f:
             f.write(result['stderr'])
             log.debug(f'{f.name} saved.')
-
-
-def termination_reason(stdout):
-    m = re.search(r'^% Termination reason: (.*)$', stdout, re.MULTILINE)
-    if m is not None:
-        return m[1]
-    return None
-
-
-termination_reason_to_status = {
-    'Refutation': 'UNS',
-    'Satisfiable': 'SAT',
-    'Time limit': 'TMO',
-    'Memory limit': 'MMO',
-    'Activation limit': 'ACO',
-    'Refutation not found, non-redundant clauses discarded': 'INC',
-    'Refutation not found, incomplete strategy': 'INC',
-    'Inappropriate': 'IAP',
-    'Unknown': 'UNK'
-}
