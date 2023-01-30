5498b7a1fdefc6819c0d43982f5f6167e30e5ef3
Filip Bartek <filip.bartek@hotmail.com>
2023-01-30 15:21:54 +0100

    Support TPTP 8 header format

--- questions/tptp.py
+++ questions/tptp.py
@@ -19,18 +19,22 @@ property_types = {
     'spc': 'category',
     'formulae': pd.UInt32Dtype(),
     'formulae_unit': pd.UInt32Dtype(),
+    'formulae_def': pd.UInt32Dtype(),
     'clauses': pd.UInt32Dtype(),
     'atoms': pd.UInt32Dtype(),
     'atoms_equality': pd.UInt32Dtype(),
+    'atoms_neg': pd.UInt32Dtype(),
     'clause_size_max': pd.UInt16Dtype(),
     'clause_size_avg': pd.UInt16Dtype(),
     'formula_depth_max': pd.UInt32Dtype(),
     'formula_depth_avg': pd.UInt32Dtype(),
     'predicates': pd.UInt32Dtype(),
+    'predicates_user': pd.UInt32Dtype(),
     'predicates_propositional': pd.UInt32Dtype(),
     'predicates_arity_min': pd.UInt16Dtype(),
     'predicates_arity_max': pd.UInt16Dtype(),
     'functors': pd.UInt32Dtype(),
+    'functors_user': pd.UInt32Dtype(),
     'functors_constant': pd.UInt32Dtype(),
     'variables': pd.UInt32Dtype(),
     'variables_singleton': pd.UInt32Dtype(),
@@ -48,16 +52,16 @@ header_patterns = {
     r'^% Rating +: (?P<rating>(\d\.\d\d|\?)) (?P<rating_release>v\d\.\d\.\d)\b': None,
     r'^% SPC +: (?P<spc>.*)$': None,
     r'^% .*\bNumber of clauses +: +(?P<clauses>\d+)\b': {'-'},
-    r'^% .*\bNumber of formulae +: +(?P<formulae>\d+) \( *(?P<formulae_unit>\d+) unit\)$': {'+'},
-    r'^% .*\bNumber of atoms +: +(?P<atoms>\d+) \( *(?P<atoms_equality>\d+) equality.*\)$': {'-', '+'},
-    r'^% .*\bMaximal clause size +: +(?P<clause_size_max>\d+) \( *(?P<clause_size_avg>\d+) average\)$': {'-'},
-    r'^% .*\bMaximal formula depth +: +(?P<formula_depth_max>\d+) \( *(?P<formula_depth_avg>\d+) average\)$': {'+'},
-    r'^% .*\bNumber of predicates +: +(?P<predicates>\d+) \( *(?P<predicates_propositional>\d+) propositional; *(?P<predicates_arity_min>\d+)-(?P<predicates_arity_max>\d+) arity\)$': {'-', '+'},
-    r'^% .*\bNumber of functors +: +(?P<functors>\d+) \( *(?P<functors_constant>\d+) constant.*\)$': {'-', '+'},
-    r'^% .*\bNumber of variables +: +(?P<variables>\d+) \( *(?P<variables_singleton>\d+) singleton\)$': {'-'},
+    r'^% .*\bNumber of formulae +: +(?P<formulae>\d+) \( *(?P<formulae_unit>\d+) (?:unit|unt)(?:; *(?P<formulae_def>\d+) def)?\)$': {'+'},
+    r'^% .*\bNumber of (?:atoms|literals) +: +(?P<atoms>\d+) \( *(?P<atoms_equality>\d+) (?:equality|equ)(?:; *(?P<atoms_neg>\d+) neg)?\)$': {'-', '+'},
+    r'^% .*\bMaximal clause size +: +(?P<clause_size_max>\d+) \( *(?P<clause_size_avg>\d+) (?:average|avg)\)$': {'-'},
+    r'^% .*\bMaximal formula depth +: +(?P<formula_depth_max>\d+) \( *(?P<formula_depth_avg>\d+) (?:average|avg)\)$': {'+'},
+    r'^% .*\bNumber of predicates +: +(?P<predicates>\d+) \((?: *(?P<predicates_user>\d+) usr;)? *(?P<predicates_propositional>\d+) (?:propositional|prp); *(?P<predicates_arity_min>\d+)-(?P<predicates_arity_max>\d+) (?:arity|aty)\)$': {'-', '+'},
+    r'^% .*\bNumber of functors +: +(?P<functors>\d+) \((?: *(?P<functors_user>\d+) usr;)? *(?P<functors_constant>\d+) (?:constant|con).*\)$': {'-', '+'},
+    r'^% .*\bNumber of variables +: +(?P<variables>\d+) \( *(?P<variables_singleton>\d+) (?:singleton|sgn)\)$': {'-'},
     # https://stackoverflow.com/a/6576808/4054250
-    r'^% .*\bNumber of variables +: +(?P<variables>\d+) \( *(?P<variables_singleton>\d+) (singleton|sgn);? *(?P<variables_universal>\d+) +!;? *(?P<variables_existential>\d+) +\?\)$': {'+'},
-    r'^% .*\bMaximal term depth +: +(?P<term_depth_max>\d+) \( *(?P<term_depth_avg>\d+) average\)$': {'-', '+'}
+    r'^% .*\bNumber of variables +: +(?P<variables>\d+) \((?: *(?P<variables_singleton>\d+) (?:singleton|sgn);?)? *(?P<variables_universal>\d+) +!;? *(?P<variables_existential>\d+) +\?\)$': {'+'},
+    r'^% .*\bMaximal term depth +: +(?P<term_depth_max>\d+) \( *(?P<term_depth_avg>\d+) (?:average|avg)\)$': {'-', '+'}
 }
 
 
@@ -88,10 +92,12 @@ def match_and_cast(pattern, string):
     if m is None:
         warnings.warn(f'Match failed on pattern {pattern}.')
         return {}
-    return {name: cast_value(name, string_value) for name, string_value in m.groupdict().items()}
+    return {name: cast_value(name, string_value) for name, string_value in m.groupdict().items() if string_value is not None}
 
 
 def cast_value(name, raw_value):
+    if raw_value is None:
+        raise ValueError(f'Attempting to cast value None of parameter {name}.')
     try:
         property_type = property_types[name]
         if property_type in ('object', 'category'):
