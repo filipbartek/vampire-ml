8fc65e47044fce776333b62736ad1eb79a80a4ab
Filip Bartek <filip.bartek@hotmail.com>
2023-02-28 18:15:37 +0100

    Allow specifying problems as paths

--- weight/tptp.py
+++ weight/tptp.py
@@ -2,7 +2,7 @@ import os
 import re
 
 
-file_name_pattern = r'^(?P<name>(?P<domain>[A-Z]{3})(?P<number>[0-9]{3})(?P<form_symbol>[-+^=_])(?P<version>[1-9])(?P<size_parameters>[0-9]*(\.[0-9]{3})*))(?:\.[pg])?$'
+file_name_pattern = r'^(?P<dirname>.*/)?(?P<name>(?P<domain>[A-Z]{3})(?P<number>[0-9]{3})(?P<form_symbol>[-+^=_])(?P<version>[1-9])(?P<size_parameters>[0-9]*(\.[0-9]{3})*))(?:\.[pg])?$'
 
 
 def problem_path(name, tptp_path):
